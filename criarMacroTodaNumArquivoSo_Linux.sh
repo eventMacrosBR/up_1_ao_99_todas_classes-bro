982f8609b8b424ae08a7e1d43d1239d9176f8b08
Nipodemos <a.lanzi@hotmail.com>
2017-11-28 23:54:37 -0200

    atualização feita por @vitorsilverio

--- criarMacroTodaNumArquivoSo_Linux.sh
+++ criarMacroTodaNumArquivoSo_Linux.sh
@@ -4,5 +4,4 @@ for i in comum/*.pm; do cat $i >> eventMacros.txt; done
 echo "DIGITE A CLASSE DESEJADA PARA GERAR O SCRIPT:"
 ls -1 classes
 read CLASSE
-for %%f in ($CLASSE\*.pm) do type "%%f" >> "eventMacros.txt"
-
+for i in classes/$CLASSE/*.pm; do cat $i >> eventMacros.txt; done
--- criarMacroTodaNumArquivoSo_Windows.bat
+++ criarMacroTodaNumArquivoSo_Windows.bat
@@ -5,5 +5,4 @@ for %%f in (comum\*.pm) do type "%%f" >> "eventMacros.txt"
 echo DIGITE A CLASSE DESEJADA PARA GERAR O SCRIPT:
 DIR /B classes
 set /p CLASSE=
-for %%f in (%CLASSE%\*.pm) do type "%%f" >> "eventMacros.txt"
-
+for %%f in (classes\%CLASSE%\*.pm) do type "%%f" >> "eventMacros.txt"
