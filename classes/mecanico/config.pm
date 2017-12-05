683959ea54b379a1fc6b7a6f3783e298397c33c8
Nipodemos <a.lanzi@hotmail.com>
2017-12-05 01:20:27 -0200

    adicionado aspas onde faltava no RG e mecanico

--- classes/mecanico/config.pm
+++ classes/mecanico/config.pm
@@ -24,15 +24,15 @@ sub extrairMapasDeUp {
 
     $variaveisDeUp = 
     [
-        {lvlMin => 1 , lvlMax => 11, lockMap => pay_fild01, saveMap => payon},
-        {lvlMin => 12, lvlMax => 25, lockMap => pay_fild08, saveMap => payon},
-        {lvlMin => 26, lvlMax => 40, lockMap => pay_dun00,  saveMap => payon},
-        {lvlMin => 41, lvlMax => 45, lockMap => pay_fild09, saveMap => payon},
-        {lvlMin => 46, lvlMax => 53, lockMap => iz_dun00,   saveMap => izlude},
-        {lvlMin => 54, lvlMax => 59, lockMap => iz_dun02,   saveMap => izlude},
-        {lvlMin => 60, lvlMax => 70, lockMap => moc_fild17, saveMap => morocc},
-        {lvlMin => 71, lvlMax => 79, lockMap => mjolnir_11, saveMap => prontera},
-        {lvlMin => 80, lvlMax => 98, lockMap => gef_fild06, saveMap => geffen}
+        {lvlMin => 1 , lvlMax => 11, lockMap => 'pay_fild01', saveMap => 'payon'},
+        {lvlMin => 12, lvlMax => 25, lockMap => 'pay_fild08', saveMap => 'payon'},
+        {lvlMin => 26, lvlMax => 40, lockMap => 'pay_dun00',  saveMap => 'payon'},
+        {lvlMin => 41, lvlMax => 45, lockMap => 'pay_fild09', saveMap => 'payon'},
+        {lvlMin => 46, lvlMax => 53, lockMap => 'iz_dun00',   saveMap => 'izlude'},
+        {lvlMin => 54, lvlMax => 59, lockMap => 'iz_dun02',   saveMap => 'izlude'},
+        {lvlMin => 60, lvlMax => 70, lockMap => 'moc_fild17', saveMap => 'morocc'},
+        {lvlMin => 71, lvlMax => 79, lockMap => 'mjolnir_11', saveMap => 'prontera'},
+        {lvlMin => 80, lvlMax => 98, lockMap => 'gef_fild06', saveMap => 'geffen'}
     ];
 	
 	foreach my $configs (@{$variaveisDeUp}) {
