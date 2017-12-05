9ead076c4aa41648598e31e79795d4e43837d06a
Nipodemos <a.lanzi@hotmail.com>
2017-12-05 00:54:17 -0200

    adicionando aspas onde é necessario

--- classes/bioquimico/quest-eden.pm
+++ classes/bioquimico/quest-eden.pm
@@ -1,7 +1,7 @@
 sub initParamsQuestEden {
     my %paramsQuestEden = (
-        armaLevel26e40 => maça,
-        armaLevel60 => machado
+        armaLevel26e40 => 'maça',
+        armaLevel60 => 'machado'
     );
     my $eventMacro = $eventMacro::Data::eventMacro;
     $eventMacro->set_full_hash('paramsQuestEden', \%paramsQuestEden);
--- classes/renegado/quest-eden.pm
+++ classes/renegado/quest-eden.pm
@@ -1,7 +1,7 @@
 sub initParamsQuestEden {
     my %paramsQuestEden = (
-        armaLevel26e40 => adaga,
-        armaLevel60 => adaga
+        armaLevel26e40 => 'adaga',
+        armaLevel60 => 'adaga'
     );
     my $eventMacro = $eventMacro::Data::eventMacro;
     $eventMacro->set_full_hash('paramsQuestEden', \%paramsQuestEden);
