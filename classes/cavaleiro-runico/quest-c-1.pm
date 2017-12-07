cdc2f6bbff1d15c2591a9645a418f965c8be5e80
Vitor <vitor.silverio.rodrigues@gmail.com>
2017-12-07 09:16:04 -0200

    Corrigindo para alfange de iniciante

--- classes/cavaleiro-runico/quest-c-1.pm
+++ classes/cavaleiro-runico/quest-c-1.pm
@@ -5,7 +5,7 @@ sub initParamsQuestClasse1 {
         mapa => 'izlude_in',
         precisaMover => 'nao',
         sequenciaConversa => 'r1 r0',
-        equipeIniciante => 13041
+        equipeIniciante => 13415
     );
     my $eventMacro = $eventMacro::Data::eventMacro;
     $eventMacro->set_full_hash('paramsQuestClasse1', \%paramsQuestClasse1);
