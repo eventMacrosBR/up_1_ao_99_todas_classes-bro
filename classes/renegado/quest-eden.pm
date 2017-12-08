11611f3664a8905ddf08572befaf7a17b53910e6
Vitor <vitorsilveriorodrigues@gmail.com>
2017-12-08 17:53:25 -0200

    adicionando quebra de linhas finais nos arquivos

--- classes/renegado/quest-eden.pm
+++ classes/renegado/quest-eden.pm
@@ -3,8 +3,8 @@ sub initParamsQuestEden {
         armaLevel26e40 => 'adaga',
         armaLevel60 => 'adaga',
         IDarmaIniciante => 13050,
-	IDarmaIntermediario => 13051,
-	IDarmaEden => 13066
+        IDarmaIntermediario => 13051,
+        IDarmaEden => 13066
     );
     my $eventMacro = $eventMacro::Data::eventMacro;
     $eventMacro->set_full_hash('paramsQuestEden', \%paramsQuestEden);
