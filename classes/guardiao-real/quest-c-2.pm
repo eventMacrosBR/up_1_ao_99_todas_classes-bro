17f34b59e12b810ede18703cfb85d7dc9a1d5b9c
Nipodemos <a.lanzi@hotmail.com>
2017-12-10 17:53:20 -0200

    Adicionando referencias para começar a quest de templário
    infelizmente essa tem cara que vai ser um bocado dificil de fazer, e vou ter que coletar todos os dados

--- classes/guardiao-real/quest-c-2.pm
+++ classes/guardiao-real/quest-c-2.pm
@@ -4,3 +4,25 @@ sub initParamsQuestClasse2 {
 	Commands::run("iconf 1041 10 1 0"); #Lampiões
 	Commands::run("iconf 2608 1 1 0" ); #Rosário 
 }
+
+#Referencias pra quest de classe 2
+
+#Templario
+#NPC'S
+#Templário Sênior prt_castle 45 169
+#Homem Angustiado prt_castle 164 32
+#Templária prt_church 95 127
+#Bliant Piyord prt_castle 35 151
+
+#lugar pra pegar os 10 lampiões: 
+#mjo_dun02 (lento mas morre menos na teoria)
+#mjo_dun03 (rápido mas morre mais teoricamente)
+#
+#lugar pra pegar as 10 Bandagens Estragadas
+#moc_pryd03 (melhor, mas mesmo assim é bem dificil , o ideral seria ter pots)
+
+#Agua Benta só é obtivel por meio de lojinha de players, então é necessario o uso do plugin better shopper
+#Precisa tambem de bala de guraná com certeza
+#e precisa habilitar vigor dentro daquele mapa
+#e precisa de Rosário, que pode ser comprado em: prt_church 108 124 (npc Madre) ,custo: 15k
+# o ideal é comprar pot branca que cura mais! assim tem menos chance de morrer porque vai ter que usar menos vezes a pot
