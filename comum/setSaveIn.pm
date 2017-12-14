c7c3b41374156a72c8ad7e6c89f49ca27772e6ad
Nipodemos <a.lanzi@hotmail.com>
2017-12-14 12:25:12 -0200

    Mudança temporária pra configuração do buyAuto funcionar

--- comum/setSaveIn.pm
+++ comum/setSaveIn.pm
@@ -27,37 +27,37 @@ macro SetSaveIn {
 		    do conf -f saveMap_wanted comodo
 			do conf -f saveMap_kafra_position 195 150
 			do conf -f saveMap_sellNpc_position cmd_in01 79 182
-			do conf buyAuto_0_npc cmd_in01 79 182
+			do conf buyAuto_1_npc cmd_in01 79 182
 		}
 		case (=~ /prontera/i ) {
 		    do conf -f saveMap_wanted prontera
 			do conf -f saveMap_kafra_position 151 29
 			do conf -f saveMap_sellNpc_position prontera 113 42
-			do conf buyAuto_0_npc prt_in 126 76
+			do conf buyAuto_1_npc prt_in 126 76
 		}
 		case (=~ /payon/i ) {
 			do conf -f saveMap_wanted payon
 			do conf -f saveMap_kafra_position 181 104
 			do conf -f saveMap_sellNpc_position payon_in01 5 49
-			do conf buyAuto_0_npc payon_in01 5 49
+			do conf buyAuto_1_npc payon_in01 5 49
 		}
 		case (=~ /geffen/i ) {
 			do conf -f saveMap_wanted geffen
 			do conf -f saveMap_kafra_position 120 62
 			do conf -f saveMap_sellNpc_position geffen_in 74 144
-			do conf buyAuto_0_npc geffen_in 77 167
+			do conf buyAuto_1_npc geffen_in 77 167
 		}
 		case (=~ /morocc|morroc|morooc/i ) {
 			do conf -f saveMap_wanted morocc
 			do conf -f saveMap_kafra_position 156 97
 			do conf -f saveMap_sellNpc_position morocc_in 141 67
-			do conf buyAuto_0_npc moc_ruins 114 63
+			do conf buyAuto_1_npc moc_ruins 114 63
 		}
 		case (=~ /izlude/i ) {
 			do conf -f saveMap_wanted izlude
 			do conf -f saveMap_kafra_position 134 88
 			do conf -f saveMap_sellNpc_position izlude 105 99
-			do conf buyAuto_0_npc izlude_in 115 61
+			do conf buyAuto_1_npc izlude_in 115 61
 		}
 		case ( =~ /veins/i ) {
 			do conf -f saveMap_wanted veins
