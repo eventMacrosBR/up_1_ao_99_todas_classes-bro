296388e7629e2c78ae194b94618757f5c60630b1
nipodemos <a.lanzi@hotmail.com>
2017-12-19 21:37:17 -0200

    Uma forma mais fácil de gerar o eventMacros.txt

---
+++ classes/arcebispo/gerar_eventMacro_Windows.bat
@@ -0,0 +1,5 @@
+@echo off
+DEL eventMacros.txt
+for %%f in (*.pm) do type "%%f" >> "..\..\eventMacros.txt"
+for %%f in (..\..\comum\*.pm) do type "%%f" >> "eventMacros.txt"
+echo eventMacros.txt gerado com sucesso
