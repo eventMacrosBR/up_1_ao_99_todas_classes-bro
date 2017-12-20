62d9ff7a75c3bc3f93263dbd5816fa526a4090a1
Nipodemos <a.lanzi@hotmail.com>
2017-12-20 01:03:29 -0200

    corrigindo indentação (por algum motivo tava toda errada)

--- classes/bioquimico/quest-c-2.pm
+++ classes/bioquimico/quest-c-2.pm
@@ -19,7 +19,7 @@ sub initParamsQuestClasse2 {
 #################################################################
 
 automacro virarAlquimistaInicio_IrNoNpc {
-    JobID $paramsClasses{idC1}
+	JobID $paramsClasses{idC1}
 	JobLevel = 50
 	FreeSkillPoints = 0
 	priority 2
@@ -68,32 +68,31 @@ automacro virarAlquimista_descobrindoQualItem {
 	NpcMsg /Como eu te disse, você precisa trazer/
    	exclusive 1
    	call {
-    		[
-        	log =*=*=*=*=*=*=*=*=*=*=*=*=*=*==*=*=*=
-    		log a mensagem é: $.NpcMsgLastMsg
-        	log *=*=*=*=*=*=*=*=*=*=*=*==*=*=*=*=*=*
-        	]
-        	$mensagemExtraida = extrairMensagem("$.NpcMsgLastMsg")
-    		if ( $mensagemExtraida =~ /F.ria Selvagem/) {
-        		do conf -f questAlquimista comprarPocao
-        	} elsif ( $mensagemExtraida =~ /Flecha/ ) {
-        		do conf -f questAlquimista comprarFlecha
-        	} elsif ( $mensagemExtraida =~ /Fornalha/ ) {
-        		do conf -f questAlquimista comprarFornalha
-        	} else {
-        		log erro configurado proximo passo, cheque a macro
-        	}
-    	}
+		[
+		log =*=*=*=*=*=*=*=*=*=*=*=*=*=*==*=*=*=
+		log a mensagem é: $.NpcMsgLastMsg
+		log *=*=*=*=*=*=*=*=*=*=*=*==*=*=*=*=*=*
+		]
+		$mensagemExtraida = extrairMensagem("$.NpcMsgLastMsg")
+		if ( $mensagemExtraida =~ /F.ria Selvagem/) {
+			do conf -f questAlquimista comprarPocao
+		} elsif ( $mensagemExtraida =~ /Flecha/ ) {
+			do conf -f questAlquimista comprarFlecha
+		} elsif ( $mensagemExtraida =~ /Fornalha/ ) {
+			do conf -f questAlquimista comprarFornalha
+		} else {
+			log erro configurado proximo passo, cheque a macro
+		}
+	}
 }
 
 sub extrairMensagem {
 	my ($mensagem) = @_;
    	if ($mensagem =~ /trazer (.+) para completar/) {
-    		return $1;
-   	}
-   	else {
-    		warning "ERRO, NÃO FOI POSSIVEL EXTRAIR MENSAGEM\n";
-    		return -1;
+		return $1;
+   	} else {
+		warning "ERRO, NÃO FOI POSSIVEL EXTRAIR MENSAGEM\n";
+		return -1;
    	}
 }
 
@@ -104,13 +103,13 @@ automacro virarAlquimista_comprarFornalha {
 	ConfigKey questAlquimista comprarFornalha
    	InInventoryID 612 < 100 #Mini-Fornalha
    	call {
-    		do move geffen
-        	do move geffen_in 106 178
-        	do talk &npc(110 172)
-        	do talk resp 1
-        	do talk resp 0
-        	do talk num 100
-    	}
+		do move geffen
+		do move geffen_in 106 178
+		do talk &npc(110 172)
+		do talk resp 1
+		do talk resp 0
+		do talk num 100
+	}
 }
 
 automacro virarAlquimista_comprarFuria {
@@ -169,17 +168,17 @@ macro voltarProAlquimistaSenior {
    	do talk &npc (/Alquimista sênior/)
    	do conf lockMap none
    	pause 3
-    
+	
    	#isso vai checar se realmente entregamos o item, ja que está sempre suscetivel a falhas
    	#se não tiver entregado por qualquer motivo que seja, ele simplesmente vai tentar denovo 
    	#Se conseguir, ele move pra o proximo passo
    	if ($.InInventoryIDLast = 657) { #Poção da Fúria Selvagem
-    		if (&inventory(Poção da Fúria Selvagem) < 7) do conf -f questAlquimista questionario
+		if (&inventory(Poção da Fúria Selvagem) < 7) do conf -f questAlquimista questionario
    	} elsif ($.InInventoryIDLast = 612) { #Mini-Fornalha
-    		if (&inventory(Mini-Fornalha) < 100) do conf -f questAlquimista questionario
+		if (&inventory(Mini-Fornalha) < 100) do conf -f questAlquimista questionario
    	} elsif ($.InInventoryIDLast = 1752) { #Flecha de Fogo
-    		if (&inventory(Flecha de Fogo)  < 500) do conf -f questAlquimista questionario
-    	}
+		if (&inventory(Flecha de Fogo)  < 500) do conf -f questAlquimista questionario
+	}
 }		
 
 #################################################################
@@ -195,45 +194,45 @@ automacro virarAlquistia_npcDoQuestionario {
 	exclusive 1
    	timeout 60
    	call {
-    		[
+		[
 		log ======================================================
 		log = 		Como estamos no nível de classe 50,	 =
-        	log =  não precisamos responder o questionário, somente  =
+		log =  não precisamos responder o questionário, somente  =
 		log = 				falar com ele! hehehe	 =
 		log ======================================================
 		]
-        	do move alde_alche &rand(166,168) &rand(106,108)
-        	pause 5
+		do move alde_alche &rand(166,168) &rand(106,108)
+		pause 5
 		do talk &npc (/Alquimista Misterioso/)
-    	}
+	}
 }
 
 automacro virarAlquimista_npcQuestionario_bugged {
    	NpcMsgName /.*vez que falou comigo.*/ / Misterioso/
    	ConfigKey questAlquimista questionario
    	call {
-    		do conf -f questAlquimista npcDaFlor
+		do conf -f questAlquimista npcDaFlor
    	}
 }
 
 automacro virarAlquimista_jaFaleiComNpcQuestionario {
   	exclusive 1
-    	NpcMsg /Diga a Darcya que eu te mandei/
-	ConfigKey questAlquimista questionario
-    	call {
-        	do conf -f questAlquimista npcDaFlor
-    	}
-}
-
-automacro virarAlquimista_falarComNpcFlor {
-	QuestActive 2032
+	NpcMsg /Diga a Darcya que eu te mandei/
 	ConfigKey questAlquimista questionario
 	call {
 		do conf -f questAlquimista npcDaFlor
 	}
 }
 
-    	
+automacro virarAlquimista_falarComNpcFlor {
+	QuestActive 2032
+	ConfigKey questAlquimista questionario
+	call {
+	do conf -f questAlquimista npcDaFlor
+	}
+}
+
+		
 #################################################################
 #								#
 # 			   Continuando a Quest de Alquimista!	#
@@ -243,15 +242,15 @@ automacro virarAlquimista_falarComNpcFlor {
 
 automacro virarAlquimista_NpcDaFlor {
 	ConfigKey questAlquimista npcDaFlor
-    	timeout 30
-    	exclusive 1
+	timeout 30
+	exclusive 1
 	call {
 		call pararDeAtacar
-        	do conf lockMap none
-        	do iconf 710 0 0 0 #Flor das Ilusões
+		do conf lockMap none
+		do iconf 710 0 0 0 #Flor das Ilusões
 		do move alde_alche &rand(13,16) &rand(18,20)
 		do talk &npc(/Pesquisador/)
-        	do talk resp 0
+		do talk resp 0
 	}
 }
 
@@ -265,11 +264,11 @@ automacro virarAlquimista_faleiComNpcDaFlor_bugged {
 }
 automacro virarAlquimista_faleiComNpcDaFlor {
 	ConfigKey questAlquimista npcDaFlor
-    	exclusive 1
-    	NpcMsg /Consiga os itens e volte/
-    	call {
-        do conf -f questAlquimista pegarAFlor
-    }
+		exclusive 1
+		NpcMsg /Consiga os itens e volte/
+		call {
+		do conf -f questAlquimista pegarAFlor
+	}
 }
 
 automacro virarAlquimista_naoTenhoAFlorVouPegar {
@@ -291,38 +290,38 @@ automacro virarAlquimista_naoTenhoAFlorVouPegar {
 automacro virarAlquimista_naoTenhoAFlor {
 	ConfigKey questAlquimista pegarAFlor
 	ConfigKeyNot questAlquimista fimDaQuest
-    	exclusive 1
-    	Zeny >= 300000
-    	InInventoryID 710 < 1 #Flor das Ilusões
-    	ConfigKeyNot BetterShopper_on 1
-    	call {
-    		call pararDeAtacar
-        	do conf lockMap none
-       		if ( recursoTecnicoPaliativo() = 1) { #Significa que o BetterShopper está ativado
-        		do conf lockMap prontera
-            		do conf route_randomWalk 1
-            		do conf route_randomWalk_inTown 1
-            		do conf itemsTakeAuto 1
-            		do conf itemsGatherAuto 0
-            		do conf itemsMaxWeight_sellOrStore 40
-            
-            		if (&config(BetterShopper_0) != Flor das Ilusões) {
-        			configurarBetterShopper()
-                		pause 3
-                		do reload config
-            		}
-        	} else {
-        		[
-        		log =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
-            		log *  	É preciso baixar e ativa o plugin BetterShopper.		*
-            		log =  	Até lá nem a macro nem o Openkore vão continuar!		=
-            		log * Caso já tenha feito isso, feche e abra o Openkore novamente!	*
-            		log =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
-            		]
-            		do eval Misc::offlineMode();
-            		stop
-        	}
-    	}
+	exclusive 1
+	Zeny >= 300000
+	InInventoryID 710 < 1 #Flor das Ilusões
+	ConfigKeyNot BetterShopper_on 1
+	call {
+		call pararDeAtacar
+		do conf lockMap none
+		if ( recursoTecnicoPaliativo() = 1) { #Significa que o BetterShopper está ativado
+			do conf lockMap prontera
+				do conf route_randomWalk 1
+				do conf route_randomWalk_inTown 1
+				do conf itemsTakeAuto 1
+				do conf itemsGatherAuto 0
+				do conf itemsMaxWeight_sellOrStore 40
+		
+				if (&config(BetterShopper_0) != Flor das Ilusões) {
+				configurarBetterShopper()
+					pause 3
+					do reload config
+				}
+		} else {
+			[
+			log =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+				log *  	É preciso baixar e ativa o plugin BetterShopper.		*
+				log =  	Até lá nem a macro nem o Openkore vão continuar!		=
+				log * Caso já tenha feito isso, feche e abra o Openkore novamente!	*
+				log =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+				]
+				do eval Misc::offlineMode();
+				stop
+		}
+	}
 }
 
 automacro virarAlquimista_naoTenhoAFlor_bugged {
@@ -341,85 +340,85 @@ automacro virarAlquimista_naoTenhoAFlor_bugged {
 
 sub configurarBetterShopper {
 	open (my $write, '>>:encoding(UTF-8)', Settings::getControlFilename('config.txt'));
-    	print $write "BetterShopper_on 1\n";
+	print $write "BetterShopper_on 1\n";
 	print $write "BetterShopper Flor das Ilusões {\n";
 	print $write "maxPrice 350000\n";
 	print $write "maxAmount 1\n";
 	print $write "disabled 0\n";
 	print $write "}\n";
-    	close($fh);
+	close($fh);
 }
 
 automacro virarAlquimista_naoTenhoAFlor_NemZeny {
 	ConfigKey questAlquimista pegarAFlor
 	ConfigKeyNot questAlquimista fimDaQuest
-    	exclusive 1
-    	Zeny < 300000
-    	InInventoryID 710 < 1 #Flor das Ilusões
-    	ConfigKeyNot lockMap pay_fild09
-    	ConfigKeyNot In_saveMap_sequence true
-    	timeout 120
-    	call {
-    	[
-    	log =====================================================
-        log =Não tenho zeny suficiente para comprar a Flor	 
-        log =Então vou conseguir o dinheiro!				 
-        log =Começando o processo de vender loot e conseguir mais
-        log =====================================================
-        ]
-        do autosell
-        do iconf 690 0 0 1 #Lanças de Fogo Nível 3
-        do iconf 757 0 0 1 #Minério de Elunium
-        do iconf 907 0 0 1 #Resina
-        do iconf 947 0 0 1 #Chifre
-        do iconf 914 0 0 1 #Felpa
-        do iconf 919 0 0 1 #Couro de Animal
-        do iconf 943 0 0 1 ##Casca Rija
-        do iconf 935 0 0 1 #Casca
-        do iconf 1011 0 0 1 #Emveretarcon
-        do iconf 2329 0 0 1 #Armadura de Madeira
-        do iconf 1026 0 0 1 #Avelã
-        do iconf 2502 0 0 1 #Capuz [1]
-        do iconf 2402 0 0 1 #Sandálias [1]
-        
-        call voltarAtacar
-        call SetSaveIn "payon" 
-	do conf lockMap pay_fild09
-    }
+		exclusive 1
+		Zeny < 300000
+		InInventoryID 710 < 1 #Flor das Ilusões
+		ConfigKeyNot lockMap pay_fild09
+		ConfigKeyNot In_saveMap_sequence true
+		timeout 120
+		call {
+		[
+		log =====================================================
+		log =Não tenho zeny suficiente para comprar a Flor	 
+		log =Então vou conseguir o dinheiro!				 
+		log =Começando o processo de vender loot e conseguir mais
+		log =====================================================
+		]
+		do autosell
+		do iconf 690 0 0 1 #Lanças de Fogo Nível 3
+		do iconf 757 0 0 1 #Minério de Elunium
+		do iconf 907 0 0 1 #Resina
+		do iconf 947 0 0 1 #Chifre
+		do iconf 914 0 0 1 #Felpa
+		do iconf 919 0 0 1 #Couro de Animal
+		do iconf 943 0 0 1 ##Casca Rija
+		do iconf 935 0 0 1 #Casca
+		do iconf 1011 0 0 1 #Emveretarcon
+		do iconf 2329 0 0 1 #Armadura de Madeira
+		do iconf 1026 0 0 1 #Avelã
+		do iconf 2502 0 0 1 #Capuz [1]
+		do iconf 2402 0 0 1 #Sandálias [1]
+		
+		call voltarAtacar
+		call SetSaveIn "payon" 
+		do conf lockMap pay_fild09
+	}
 }
 
 sub recursoTecnicoPaliativo {
 	my $estaNoSys;
-    	my $controlfile = Settings::getControlFilename('sys.txt');
+	my $controlfile = Settings::getControlFilename('sys.txt');
 	open(FILE, '<:encoding(UTF-8)', $controlfile);
 	my @lines = <FILE>;
 	close(FILE);
 	chomp @lines;
-    
+	
 	foreach my $line (@lines) {
 		next if $line =~ /^$/ || $line =~ /^#/;
 		if ($line =~ /BetterShopper/ ) {
 			$estaNoSys = 1 ;
-           		last;
+		   	last;
 		}
 	}
-    
-    	if ($estaNoSys == 1 ) {
-    		return 1;
-    	} else {
-    		return -1;
+	
+	if ($estaNoSys == 1 ) {
+		return 1;
+	} else {
+		return -1;
    	}
 }
 
 automacro virarAlquimista_juntandoZeny {
 	ConfigKey questAlquimista pegarAFlor
 	ConfigKeyNot questAlquimista fimDaQuest
-    	exclusive 1
-    	ConfigKey lockMap pay_fild09
-    	ConfigKey saveMap payon
-    	timeout 180
-    	call {
-    	[
+	exclusive 1
+	ConfigKey lockMap pay_fild09
+	ConfigKey saveMap payon
+	timeout 180
+	call {
+		[
 		log ======================================
 		log =Estou coletando items pra fazer zeny
 		log =Para Quest de Alquimista		 
@@ -429,14 +428,14 @@ automacro virarAlquimista_juntandoZeny {
 		if (&config(itemsTakeAuto) != 2) do conf itemsTakeAuto 2
 		if (&config(itemsGatherAuto) != 2) do conf itemsGatherAuto 2
 		if (&config(itemsMaxWeight_sellOrStore) != 30) do conf itemsMaxWeight_sellOrStore 30
-    	}
+	}
 }
 
 
 automacro virarAlquimista_euTenhoAFlor {
 	ConfigKey questAlquimista pegarAFlor
 	InInventoryID 710 >= 1 #Flor das Ilusões
-    	timeout 60
+	timeout 60
 	call {
 		do conf -f questAlquimista jaPegueiAFlor
 	}
@@ -458,32 +457,32 @@ automacro virarAlquimista_indoEntregaAFlor {
 
 automacro virarAlquimista_entregueiAFlor {
 	ConfigKey questAlquimista jaPegueiAFlor
-    	exclusive 1
-    	NpcMsgName /torne-se um Alquimista de verdade/ /Pesquisador/
-    	priority -5 #alto
-    	call {
+	exclusive 1
+	NpcMsgName /torne-se um Alquimista de verdade/ /Pesquisador/
+	priority -5 #alto
+	call {
 		pause 1
 		do conf -f questAlquimista fimDaQuest
-    	}
+	}
 }
 
 automacro virarAlquimista_entregueiAFlor_bugged {
 	exclusive 1
    	NpcMsgName /de um Alquimista/ /Pesquisador/
-    	ConfigKey questAlquimista jaPegueiAFlor
-    	call {
+	ConfigKey questAlquimista jaPegueiAFlor
+	call {
 		do conf -f questAlquimista fimDaQuest
-    	}
+	}
 }
 
 automacro virarAlquimista_entregueiAFlor_bugged2 {
 	exclusive 1
 	ConfigKey questAlquimista jaPegueiAFlor
 	InInventoryID 710 < 1 #Flor das Ilusões
-    	call {
+	call {
 		#se isso ativar, significa que entregamos a flor
 		do conf -f questAlquimista fimDaQuest
-    	}
+	}
 }
 
 #################################################################
@@ -504,7 +503,7 @@ automacro virarAlquimista_FimDaQuest_FalarComAlquimistaChefe {
 		[
 		log ==============================
 		log =Finalmente virei Alquimista!=
-			log ==============================
+		log ==============================
 		]
    	}
 }
