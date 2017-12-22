6d660416235f84e6a05005c1fa962a90bc18156a
Nipodemos <a.lanzi@hotmail.com>
2017-12-22 01:38:26 -0200

    fix virgula esquecida

--- classes/guardiao-real/param.pm
+++ classes/guardiao-real/param.pm
@@ -2,7 +2,7 @@ sub initParamsClasses {
     my %paramsClasses = (
         idC1 => 1,
         idC2 => 14,
-        idC2Alt => 21
+        idC2Alt => 21,
         idC1T => 4002,
         idC2T => '4015,4022',
         idC3 => '4066,4073'
