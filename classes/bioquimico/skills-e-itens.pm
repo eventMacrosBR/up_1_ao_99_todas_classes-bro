1355fc615f57bf8f4e23fadee98a546214ace990
Nipodemos <a.lanzi@hotmail.com>
2017-12-24 12:49:06 -0200

    desabilita e habilida pot laranja pra alquimista

---
+++ classes/bioquimico/skills-e-itens.pm
@@ -0,0 +1,24 @@
+#Desabilitar pot laranja enquanto for classe 1
+
+automacro desabilitarPotLaranja {
+	ConfigKey buyAuto_1 502
+	ConfigKeyNot buyAuto_0_disabled 1
+	JobID $paramsClasses{idC1}
+	exclusive 1
+	call {
+		log DESABILITANDO COMPRA DE POÇÃO LARANJA
+		log PARA ECONOMIZAR DINHEIRO PRA QUEST 2
+		do conf buyAuto_0_disabled 1
+	}
+}
+
+automacro habilitarNovamentePotLaranja {
+	ConfigKey buyAuto_1 502
+	ConfigKeyNot buyAuto_0_disabled 1
+	JobID $paramsClasses{idC2}
+	exclusive 1
+	call {
+		log HABILITANDO COMPRA DE POÇÃO LARANJA
+		do conf buyAuto_0_disabled 0
+	}
+}
