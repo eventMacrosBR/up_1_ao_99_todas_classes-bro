e0226f76c01a67f01301d97b84bab0e14a5cbbff
Nipodemos <a.lanzi@hotmail.com>
2017-12-24 12:39:18 -0200

    adicionando tabs

--- classes/bioquimico/quest-eden.pm
+++ classes/bioquimico/quest-eden.pm
@@ -3,8 +3,8 @@ sub initParamsQuestEden {
         armaLevel26e40 => 'maça',
         armaLevel60 => 'machado',
         IDarmaIniciante => 16004,
-	IDarmaIntermediario => 16005,
-	IDarmaEden => 1391
+	    IDarmaIntermediario => 16005,
+	    IDarmaEden => 1391
     );
     my $eventMacro = $eventMacro::Data::eventMacro;
     $eventMacro->set_full_hash('paramsQuestEden', \%paramsQuestEden);
