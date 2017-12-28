2a9965154fbec884e87aba51ba63134966243018
Nipodemos <nipodemos13@gmail.com>
2017-12-27 22:01:43 -0200

    deixando o código ainda menor

--- classes/arcebispo/quest-c-2.pm
+++ classes/arcebispo/quest-c-2.pm
@@ -7,7 +7,7 @@ sub initParamsQuestClasse2 {
 #ta em macro plugin é so reescrever algumas partes pra eventMacros
 #Alterei pro IRO RESTART
 #Possivel conflito com o BRO autoTalkCont 1 ainda funciona no IRO
-automacro Virar Sacer 1 {
+automacro VirarSacer1 {
     JobID $paramsClasses{idC1}
     JobLevel = 50
     priority 2
@@ -21,7 +21,7 @@ automacro Virar Sacer 1 {
         do move prt_church 16 39
         do talknpc 16 41 c r0 c r0 c r0 c  #Bispo da Guilda
         pause 60
-        lock Virar Sacer 1
+        lock VirarSacer1
     }
 }
 
@@ -30,35 +30,26 @@ automacro Virar Sacer 2 {
     InMap job_prist
     exclusive 1
     timeout 60
+    macro_delay 4
     call {
-        lock Virar Sacer 1
-        pause 60
-        do talknpc 24 187 c r0 c r0 c #Smith
+        lock VirarSacer1
+        pause 60 #sera que isso é necessario?
+        do talknpc 24 187 r0 r0 #Smith
         do conf attackAuto 2
         do conf attackUseWeapon 1
         do talk cont
         do move job_prist 24 109
-        pause 3
         do move job_prist 168 49
         do talk resp 2
-        pause 5
         do move job_prist 169 80
-        pause 5
         do talk resp 2
         do move job_prist 168 119
-        pause 5
         do talk resp 2
-        pause 5
         do move job_prist 168 157
-        pause 5
         do talk resp 2
-        pause 2
         do move job_prist 168 178
-        pause 3
         do move job_prist 97 105
-        pause 1
-        do talknpc 27 24 c r0 c r1 c r0 c r0 c r1 c r1 c r0 c r0 c
-        pause 1
-        do talknpc 16 41 c
+        do talknpc 27 24 r0 r1 r0 r0 r1 r1 r0 r0
+        do talknpc 16 41 
     }
 }
