ae49b76a70256ea673c53deb0090279c9fa530eb
Vitor <vitorsilveriorodrigues@gmail.com>
2017-12-29 00:02:53 -0200

    Adicionando quests classes 2T que faltavam

---
+++ classes/sentinela/quest-c-2T.pm
@@ -0,0 +1,7 @@
+sub initParamsQuestClasse2T {
+    my %paramsQuestClasse2T = (
+        Npc => '44 55'
+    );
+    my $eventMacro = $eventMacro::Data::eventMacro;
+    $eventMacro->set_full_hash('paramsQuestClasse2T', \%paramsQuestClasse2T);
+}
