3e84091b838c4962e862e3c0ba627e0f94d99716
Vitor <vitorsilveriorodrigues@gmail.com>
2017-12-29 13:55:56 -0200

    corrigindo configuração de buyAuto de poção laranja para usar o nome em vez do ID e também colocando no buyAuto_1 em vez do 0, que é onde está efetivamente configuado

--- classes/bioquimico/skills-e-itens.pm
+++ classes/bioquimico/skills-e-itens.pm
@@ -1,25 +1,25 @@
 #Desabilitar pot laranja enquanto for classe 1
 
 automacro desabilitarPotLaranja {
-	ConfigKey buyAuto_1 502
-	ConfigKeyNot buyAuto_0_disabled 1
+	ConfigKey buyAuto_1 Poção Laranja
+	ConfigKeyNot buyAuto_1_disabled 1
 	JobID $paramsClasses{idC1}
 	exclusive 1
 	call {
 		log DESABILITANDO COMPRA DE POÇÃO LARANJA
 		log PARA ECONOMIZAR DINHEIRO PRA QUEST 2
-		do conf buyAuto_0_disabled 1
+		do conf buyAuto_1_disabled 1
 	}
 }
 
 automacro habilitarNovamentePotLaranja {
-	ConfigKey buyAuto_1 502
-	ConfigKeyNot buyAuto_0_disabled 1
+	ConfigKey buyAuto_1 Poção Laranja
+	ConfigKeyNot buyAuto_1_disabled 1
 	JobID $paramsClasses{idC2}
 	exclusive 1
 	call {
 		log HABILITANDO COMPRA DE POÇÃO LARANJA
-		do conf buyAuto_0_disabled 0
+		do conf buyAuto_1_disabled 0
 	}
 }
 
