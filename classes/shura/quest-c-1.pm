cfbd8faa221dd228dbe66d0e3de30c6564124b47
Vitor <vitorsilveriorodrigues@gmail.com>
2017-12-28 22:22:49 -0200

    Adicionando quest de noviço para arcebispo e shura

---
+++ classes/shura/quest-c-1.pm
@@ -0,0 +1,13 @@
+sub initParamsQuestClasse1 {
+    my %paramsQuestClasse1 = (
+        nomeClasse => 'noviço',
+        npc => '173 23',
+        mapa => 'prt_church',
+        precisaMover => 'sim',
+        sequenciaConversa => 'r0',
+        equipeIniciante => 1545
+    );
+    my $eventMacro = $eventMacro::Data::eventMacro;
+    $eventMacro->set_full_hash('paramsQuestClasse1', \%paramsQuestClasse1);
+}
+
