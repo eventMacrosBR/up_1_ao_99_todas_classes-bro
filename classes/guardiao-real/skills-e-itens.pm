83ca4c683d0332757982b0f1882dda6a2c1f17b0
Nipodemos <a.lanzi@hotmail.com>
2017-12-29 23:51:15 -0200

    adicionado teste

--- classes/guardiao-real/skills-e-itens.pm
+++ classes/guardiao-real/skills-e-itens.pm
@@ -44,8 +44,11 @@ automacro configurarPotBranca {
 	ConfigKeyNot useSelf_item_1 504 #Poção Branca
 	exclusive 1
 	call {
-		adicionaUseSelfItem()
-		do reload config
+		$blocoExiste = checarSeExisteNoConfig("useSelf_item_1")
+		if ($blocoExiste = nao ) {
+			adicionaUseSelfItem()
+			do reload config
+		}
 		do conf useSelf_item_1 504 #Poção Branca
 		do conf useSelf_item_1_hp < 60%
 		do conf useSelf_item_1_disabled 0
