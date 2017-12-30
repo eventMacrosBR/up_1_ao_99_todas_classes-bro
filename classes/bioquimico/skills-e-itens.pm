731bb4ec087f4c504bef161170fad791f135f06c
Nipodemos <a.lanzi@hotmail.com>
2017-12-30 01:08:26 -0200

    removendo automacro duplicada

--- classes/bioquimico/skills-e-itens.pm
+++ classes/bioquimico/skills-e-itens.pm
@@ -1,26 +1,3 @@
 #Desabilitar pot laranja enquanto for classe 1
 
-automacro desabilitarPotLaranja {
-	ConfigKey buyAuto_1 Poção Laranja
-	ConfigKeyNot buyAuto_1_disabled 1
-	JobID $paramsClasses{idC1}
-	exclusive 1
-	call {
-		log DESABILITANDO COMPRA DE POÇÃO LARANJA
-		log PARA ECONOMIZAR DINHEIRO PRA QUEST 2
-		do conf buyAuto_1_disabled 1
-	}
-}
-
-automacro habilitarNovamentePotLaranja {
-	ConfigKey buyAuto_1 Poção Laranja
-	ConfigKeyNot buyAuto_1_disabled 1
-	JobID $paramsClasses{idC2}
-	exclusive 1
-	call {
-		log HABILITANDO COMPRA DE POÇÃO LARANJA
-		do conf buyAuto_1_disabled 0
-	}
-}
-
 
