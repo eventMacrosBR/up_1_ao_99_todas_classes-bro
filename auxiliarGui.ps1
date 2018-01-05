344ad1c40b5e4ceac09dc68b592c54682e73fc09
Vitor Silverio <vitor.silverio.rodrigues@gmail.com>
2018-01-05 00:21:06 -0200

    Primeira grande refatoração do programa que gera a macro

--- auxiliarGui.ps1
+++ auxiliarGui.ps1
@@ -1,74 +1,37 @@
 ﻿Add-Type -AssemblyName System.Windows.Forms
 
 $Form = New-Object system.Windows.Forms.Form
-$Form.Text = "Gerador eventMacros.txt"
-$Form.TopMost = $true
-$Form.Width = 450
-$Form.Height = 100
+$cbxJobs = New-Object system.windows.Forms.ComboBox
+$btn = New-Object system.windows.Forms.Button
 
-$version = "0000000"
-
-try {
-    $hash = (git rev-parse HEAD) | Out-String
-    $hash = $hash.substring(0,7)
-    $commitCounter = (git rev-list --count master) | Out-String 
-    $commitCounter = $commitCounter -replace "\r\n" 
-    $version = $commitCounter + "." + $hash 
-    
-}catch{
-    Write-Out "Git não instalado, não vai ser exibida a versão"
+function getVersao(){
+    $version = "versao_indefinida"
+    try {
+        $hash = (git rev-parse HEAD) | Out-String
+        $hash = $hash.substring(0,7)
+        $commitCounter = (git rev-list --count master) | Out-String 
+        $commitCounter = $commitCounter -replace "\r\n" 
+        $version = $commitCounter + "." + $hash 
+        
+    }catch{
+        Write-Out "Git não instalado, não vai ser exibida a versão"
+    }
+    return $version
 }
 
-$Form.Text = $Form.Text + " versão: " + $version
-$cbxJobs = New-Object system.windows.Forms.ComboBox
-$cbxJobs.Width = 300
-$cbxJobs.location = new-object system.drawing.point(10,20)
-$cbxJobs.Items.Add("Arcano")
-$cbxJobs.Items.Add("Arcebispo")
-$cbxJobs.Items.Add("Bioquímico")
-$cbxJobs.Items.Add("Cavaleiro Rúnico")
-$cbxJobs.Items.Add("Feiticeiro")
-$cbxJobs.Items.Add("Guardião Real")
-$cbxJobs.Items.Add("Mecânico")
-$cbxJobs.Items.Add("Musa")
-$cbxJobs.Items.Add("Renegado")
-$cbxJobs.Items.Add("Sentinela")
-$cbxJobs.Items.Add("Sicário")
-$cbxJobs.Items.Add("Shura")
-$cbxJobs.Items.Add("Trovador")
-$Form.controls.Add($cbxJobs)
-
-$btn = New-Object system.windows.Forms.Button
-$btn.Text = "Gerar"
-$btn.Width = 60
-$btn.location = new-object system.drawing.point(310,20)
-$Form.controls.Add($btn)
-
-$btn.Add_click({
+function gerarMacro(){
     $job = $cbxJobs.SelectedItem
     if($job){
         $eventMacros =  "eventMacros.txt"
+        #Remover o arquivo antigo
         if (Test-Path $eventMacros) {
           Remove-Item $eventMacros
         }
+        $versao = getVersao
         $jobSimples = $job.ToString().ToLower().Replace(" ","-").Replace("í","i").Replace("ú","u").Replace("â","a").Replace("ã","a").Replace("á","a")
-        "automacro versao { " | Out-File $eventMacros -Encoding UTF8 -append
-        "    BaseLevel > 0 " | Out-File $eventMacros -Encoding UTF8 -append
-        "    priority -999 " | Out-File $eventMacros -Encoding UTF8 -append
-        "    run-once 1 " | Out-File $eventMacros -Encoding UTF8 -append
-        "    call { " | Out-File $eventMacros -Encoding UTF8 -append
-        "        [ " | Out-File $eventMacros -Encoding UTF8 -append
-        "        log ================================================================= " | Out-File $eventMacros -Encoding UTF8 -append
-        "        log   EventMacrosBR /               " | Out-File $eventMacros -Encoding UTF8 -append
-        "        log                  up_1_ao_99_todas_classes-bro  " | Out-File $eventMacros -Encoding UTF8 -append
-        "        log   Versão:  " + $version  | Out-File $eventMacros -Encoding UTF8 -append
-        "        log   https://github.com/eventMacrosBR/up_1_ao_99_todas_classes-bro " | Out-File $eventMacros -Encoding UTF8 -append
-        "        log ================================================================= " | Out-File $eventMacros -Encoding UTF8 -append
-        "        ] " | Out-File $eventMacros -Encoding UTF8 -append
-        "        do conf -f versao_eventmacro_up_todas_as_classes_bro " + $version | Out-File $eventMacros -Encoding UTF8 -append
-        "    } " | Out-File $eventMacros -Encoding UTF8 -append
-        "} " | Out-File $eventMacros -Encoding UTF8 -append
-        " " | Out-File $eventMacros -Encoding UTF8 -append
+        $automacroVersao = Get-Content -Encoding UTF8 versao.pm 
+        $automacroVersao = $automacroVersao -replace "<versao>",$versao
+        $automacroVersao | Out-File $eventMacros -Encoding UTF8 -append 
         Get-Content -Encoding UTF8 classes\$jobSimples\*.pm | Out-File $eventMacros -Encoding UTF8 -append
         Get-Content -Encoding UTF8 comum\*.pm | Out-File $eventMacros -Encoding UTF8 -append
         [System.Windows.Forms.MessageBox]::Show("eventMacros.txt para "+$job+" gerado com sucesso!" , "Ok")
@@ -76,8 +39,53 @@ $btn.Add_click({
     } else{
         [System.Windows.Forms.MessageBox]::Show("Erro, nenhum item selecionado", "Selecione uma classe")
     }
-})
+}
 
+function desenharJanela(){
+    $versao = getVersao
+    $Form.Text = "Gerador eventMacros.txt versão: " + $versao
+    $Form.TopMost = $true
+    $Form.Width = 450
+    $Form.Height = 100
+    
+    $cbxJobs.Width = 300
+    $cbxJobs.location = new-object system.drawing.point(10,20)
+    $Form.controls.Add($cbxJobs)
 
-[void]$Form.ShowDialog()
-$Form.Dispose()
+    $btn.Text = "Gerar"
+    $btn.Width = 60
+    $btn.location = new-object system.drawing.point(310,20)
+    $Form.controls.Add($btn)
+    $Form.AcceptButton = $btn
+
+    $btn.Add_click({ gerarMacro })
+}
+
+function carregarValores(){
+    $cbxJobs.Items.Add("Arcano")
+    $cbxJobs.Items.Add("Arcebispo")
+    $cbxJobs.Items.Add("Bioquímico")
+    $cbxJobs.Items.Add("Cavaleiro Rúnico")
+    $cbxJobs.Items.Add("Feiticeiro")
+    $cbxJobs.Items.Add("Guardião Real")
+    $cbxJobs.Items.Add("Mecânico")
+    $cbxJobs.Items.Add("Musa")
+    $cbxJobs.Items.Add("Renegado")
+    $cbxJobs.Items.Add("Sentinela")
+    $cbxJobs.Items.Add("Sicário")
+    $cbxJobs.Items.Add("Shura")
+    $cbxJobs.Items.Add("Trovador")
+}
+
+function mostrarJanela() {
+    [void]$Form.ShowDialog()
+}
+
+function encerrarAplicacao(){
+    $Form.Dispose()
+}
+
+desenharJanela
+carregarValores
+mostrarJanela
+encerrarAplicacao
---
+++ versao.pm
@@ -0,0 +1,17 @@
+automacro versao { 
+    BaseLevel > 0 
+    priority -999 
+    run-once 1 
+    call { 
+        [ 
+        log ================================================================= 
+        log   EventMacrosBR /               
+        log                  up_1_ao_99_todas_classes-bro  
+        log   Versão: <versao>
+        log   https://github.com/eventMacrosBR/up_1_ao_99_todas_classes-bro 
+        log ================================================================= 
+        ] 
+        do conf -f versao_eventmacro_up_todas_as_classes_bro <versao>
+    } 
+} 
+ 
