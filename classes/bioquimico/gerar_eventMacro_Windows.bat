750840793c8fa760deac28036fd08195d0dc47e5
Vitor Silverio <vitor.silverio.rodrigues@gmail.com>
2018-01-04 23:45:56 -0200

    Removendo arquivos não mais utilizados

--- classes/bioquimico/gerar_eventMacro_Windows.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-DEL eventMacros.txt
-for %%f in (*.pm) do type "%%f" >> "eventMacros.txt"
-for %%f in (..\..\comum\*.pm) do type "%%f" >> "eventMacros.txt"
-echo eventMacros.txt gerado com sucesso
