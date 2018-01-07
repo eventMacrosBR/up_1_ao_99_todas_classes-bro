6f251f0a5bca94f768fad6cc75fd2943acec814f
Vitor Silverio <vitor.silverio.rodrigues@gmail.com>
2018-01-07 03:19:12 -0200

    terceira tentativa build automatico

--- .travis/deps.sh
+++ .travis/deps.sh
@@ -1,15 +1,3 @@
 #!/bin/sh -ex
 
 docker pull ubuntu:16.04
-
-# Import the public repository GPG keys
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-
-# Register the Microsoft Ubuntu repository
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
-
-# Update the list of products
-sudo apt-get update
-
-# Install PowerShell
-sudo apt-get install -y powershell
