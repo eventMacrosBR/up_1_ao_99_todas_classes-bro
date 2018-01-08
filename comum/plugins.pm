c42a81cedda86ace582c3f34309de30dffebab93
Nipodemos <a.lanzi@hotmail.com>
2018-01-08 12:24:46 -0200

    modificado o sub que checa o xConf
    Agora o xConf vem ativado por padrão no openkore, além de ter algumas modificações, e está localizado numa pasta diferente, se o usuário nao tiver essa pasta, significa que o opnkroe está desatualizado, é só isso que é necessário checar
    
    caso esteja la mas não esteja no sys.txt , o que é possível, o processo de adicionar no sys se mantém

--- comum/plugins.pm
+++ comum/plugins.pm
@@ -3,39 +3,19 @@ sub xConfConfiguratedOrNot {
     use File::Copy;
     
     my ($estaAtualizado, $estaNaPasta, $estaNoSys);
-    my $path_xconf_na_pasta_plugins = "plugins/xconf.pl";
+    my $path_xconf_na_pasta_plugins = "plugins/xConf/xconf.pl";
     my $path_xconf_na_pasta_needsreview = "plugins/needs-review/xconf/trunk/xconf.pl";
+    
+    unlink "plugins/xConf.pl" if (-e "plugins/xconf.pl");
     
     if (-e $path_xconf_na_pasta_plugins) {
         $estaNaPasta = 1;
-        open (my $xconf_na_pasta_plugins, "<:utf8", $path_xconf_na_pasta_plugins);
-        
-        my @linhas = <$xconf_na_pasta_plugins>;
-        close ($xconf_na_pasta_plugins);
-        chomp @linhas;
-        
-        foreach my $linha (@linhas) {
-            if ($linha =~ /^use utf8;/) {
-                $estaAtualizado = 1;
-                last;
-            }
-        }
-    } 
-    else {
-        #plugin não está na pasta plugins, então é a primeira vez
-        message "plugin xconf não foi encontrado, iniciando cópia.\n";
-        copy("$path_xconf_na_pasta_needsreview", "plugins") or die "Copia falhou: $!";
-        message "xconf.pl foi copiado para a pasta plugins.\n";
+        $estaAtualizado = 1
+    } else {
+        error "Seu openkore está desatualizado, atualize-o.\n";
+        error "Devido a isso o plugin xConf poderá não funcionar corretamente\n.";
         $estaNaPasta = 0;
-    }
-
-    if ($estaNaPasta && not $estaAtualizado) {
-        message "Plugin xConf foi encontrado, porém está desatualizado.\n";
-        message "Removendo xconf obsoleto e Iniciando atualização...\n";
-        unlink $path_xconf_na_pasta_plugins;
-        copy("$path_xconf_na_pasta_needsreview", "plugins") or die "Copia falhou: $!";
-        message "xconf.pl foi copiado para a pasta plugins.\n";
-        $estaNaPasta = 0;
+        $estaAtualizado = 0;
     }
     
     my $controlfile = Settings::getControlFilename('sys.txt');
@@ -46,14 +26,14 @@ sub xConfConfiguratedOrNot {
     foreach my $line (@lines) {
         next if $line =~ /^$/ || $line =~ /^#/;
         if ($line =~ /xconf/ ) {
-            $estaNoSys = 1 ;
+            $estaNoSys = 1;
         }
     }
     if ( $estaNoSys == 0) {
         foreach my $line (@lines) {
             next if $line =~ /^$/ || $line =~ /^#/;
             if ($line =~ /loadPlugins_list\s(.*)/ ) {
-                $line = 'loadPlugins_list '.join (',',$1,"xconf");
+                $line = 'loadPlugins_list '.join (',',$1,"xConf");
             }
         }
         open(WRITE, '>:encoding(UTF-8)', $controlfile);
@@ -64,12 +44,12 @@ sub xConfConfiguratedOrNot {
     debug ("esta na pasta: ". ($estaNaPasta ? "sim\n" : "nao\n"));
     debug ("esta atualizado: ". ($estaAtualizado ? "sim\n" : "nao\n"));
     debug ("esta no sys: ". ($estaNoSys ? "sim\n" : "nao\n"));
+    
     if ($estaNaPasta && $estaNoSys && $estaAtualizado) {
         message "Checando algumas coisas...\nTudo certo.\n";
-    }
-    else {
-        message "Ativando xConf\n";
-        Commands::run("plugin load plugins/xconf.pl");
+    } elsif ($estaNaPasta && not $estaNoSys) {
+         message "Ativando xConf\n";
+        Commands::run("plugin load plugins/xConf/xConf.pl");
     }
 }
 
