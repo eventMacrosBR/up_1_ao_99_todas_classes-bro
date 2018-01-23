8b24112e890fc1280a97245041b6a52c8f8b2101
Nipodemos <nipodemos13@gmail.com>
2018-01-23 02:06:11 -0200

    agora sim eu concordo, fica dahora desse jeito

--- classes/cavaleiro-runico/skills-e-itens.pm
+++ classes/cavaleiro-runico/skills-e-itens.pm
@@ -3,6 +3,12 @@ automacro configurarGolpeFulminante {
     exclusive 1
     ConfigKeyNot attackSkillSlot_0 SM_BASH
     call {
+        $blocoExiste = checarSeExisteNoConfig("attackSkillSlot_0")
+        if ( $blocoExiste = nao ) {
+            adicionaAttackSkillSlot()
+            pause 1
+            do reload config
+        }
         do conf attackSkillSlot_0 SM_BASH
         do conf attackSkillSlot_0_lvl 10
         do conf attackSkillSlot_0_sp >= 15
@@ -12,17 +18,24 @@ automacro configurarGolpeFulminante {
     }
 }
 automacro configurarVigor {
-    SkillLevel SM_ENDURE >= 5
+    SkillLevel SM_ENDURE = 10
     ConfigKeyNot useSelf_skill_0 SM_ENDURE
     priority 0
     exclusive 1
     call {
+        $blocoExiste = checarSeExisteNoConfig("useSelf_skill_0")
+        if ( $blocoExiste = nao ) {
+            adicionaUseSelfSkill()
+            pause 1
+            do reload config
+        }
         do conf useSelf_skill_0 SM_ENDURE
         do conf useSelf_skill_0_lvl 10
         do conf useSelf_skill_0_sp >= 15%
         do conf useSelf_skill_0_whenStatusInactive EFST_POSTDELAY, Vigor
         do conf useSelf_skill_0_disabled 0
-        do conf useSelf_skill_0_onAction attack
+        do conf useSelf_skill_0_agressives > 1
+        do conf useSelf_skill_0_timeout 10
     }
 }
 
