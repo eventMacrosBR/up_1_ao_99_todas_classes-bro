559b6fe33c10754e07b11f855a9878573d85b95f
nipodemos <a.lanzi@hotmail.com>
2018-01-28 15:52:58 -0200

    adicionado buyAuto de Veins by augustogts
    Contribuição de augustogts

--- comum/setSaveIn.pm
+++ comum/setSaveIn.pm
@@ -67,6 +67,7 @@ macro SetSaveIn {
             do conf -f saveMap_wanted veins
             do conf -f saveMap_kafra_position 208 128
             do conf -f saveMap_sellNpc_position ve_in 243 303
+            do conf buyAuto_1_npc ve_in 243 303
         }
         else {
             log nome da cidade inválido, tente novamente...
