1ea8b72c9ac6e208f6075503593ec839ee0c40b2
Vitor <vitor.silverio.rodrigues@gmail.com>
2018-02-02 08:49:06 -0200

    fix bash não aceita espaços em torno do =

--- .travis/build.sh
+++ .travis/build.sh
@@ -8,9 +8,9 @@ mkdir dist
 for i in "${jobs[@]}"; do
     pwsh -File auxiliarGui.ps1 -job "$i";
     if [[ "{$jobs_ok[@]}" =~ "$i" ]]; then
-        zip_file = "$i.funcionando.zip"
+        zip_file="$i.funcionando.zip"
     else 
-        zip_file = "$i.nao_testado.zip"
+        zip_file="$i.nao_testado.zip"
     fi    
     zip $zip_file eventMacros.txt
     mv $zip_file dist/ ;
