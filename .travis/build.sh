74c46cd6495ebc4c9830e9c5437901ecf8ce97c5
Vitor <vitor.silverio.rodrigues@gmail.com>
2018-02-02 08:35:43 -0200

    fix

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,7 +5,7 @@ git fetch --unshallow
 jobs=("arcano(nao_testado)" "arcebispo(funcionando)" "bioquimico(funcionando)" "cavaleiro-runico(nao_testado)" "feiticeiro(nao_testado)" "guardiao-real(funcionando)" "mecanico(nao_testado)" "musa(nao_testado)" "renegado(funcionando)" "sentinela(nao_testado)" "sicario(nao_testado)" "shura(nao_testado)" "trovador(nao_testado)")
 mkdir dist
 for i in "${jobs[@]}"; do
-    pwsh -File auxiliarGui.ps1 -job "${i/\(\w+\)/}";
+    pwsh -File auxiliarGui.ps1 -job "${i/\\(\w+\\)/}";
     zip $i.zip eventMacros.txt
     mv $i.zip dist/ ;
 done
