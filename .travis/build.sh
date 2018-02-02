0ac8a976525c46d0f0498a008eb45d5cedc76574
Vitor <vitor.silverio.rodrigues@gmail.com>
2018-02-02 09:21:51 -0200

    removendo ; desnecessários

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,14 +6,14 @@ jobs=("arcano" "arcebispo" "bioquimico" "cavaleiro-runico" "feiticeiro" "guardia
 jobs_ok=(" arcebispo bioquimico guardiao-real renegado ")
 mkdir dist
 for i in "${jobs[@]}"; do
-    pwsh -File auxiliarGui.ps1 -job "$i";
+    pwsh -File auxiliarGui.ps1 -job "$i"
     if [[ " {$jobs_ok[@]} " =~ " $i " ]]; then
         zip_file="$i.funcionando.zip"
     else 
         zip_file="$i.nao_testado.zip"
     fi    
     zip $zip_file eventMacros.txt
-    mv $zip_file dist/ ;
+    mv $zip_file dist/ 
 done
 
 
