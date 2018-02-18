3cdcede000be092fd40cdd3a7b512ae00787038d
SarampoBolado <34985868+SarampoBolado@users.noreply.github.com>
2018-02-17 22:38:14 -0200

    skills classe 1T e 2T Arcebispo (#84)
    skills classe 1T e 2T Arcebispo

--- classes/arcebispo/config.pm
+++ classes/arcebispo/config.pm
@@ -3,8 +3,8 @@ sub configurarBuild {
         skillsAprendiz => 'NV_BASIC 9',
         skillsClasse1 => 'AL_HEAL 10, AL_INCAGI 10, AL_DP 3, AL_ANGELUS 5, AL_DP 6, AL_BLESSING 5, AL_ANGELUS 10, AL_BLESSING 10, AL_RUWACH 1, AL_HOLYWATER 1, AL_PNEUMA 1',
         skillsClasse2 => 'PR_KYRIE 4, PR_MAGNIFICAT 5, PR_GLORIA 5, PR_STRECOVERY 1, PR_KYRIE 10, PR_IMPOSITIO 5, PR_SANCTUARY 10, MG_SRECOVERY 4, ALL_RESURRECTION 4, MG_SRECOVERY 9',
-        skillsClasse1T => '',
-        skillsClasse2T => '',
+        skillsClasse1T => 'AL_HEAL 10, AL_INCAGI 10, AL_DP 3, AL_ANGELUS 5, AL_DP 5, AL_BLESSING 5, AL_ANGELUS 10, AL_BLESSING 10, AL_RUWACH 1, AL_HOLYWATER 1, AL_PNEUMA 1, AL_DECAGI 1',
+        skillsClasse2T => 'PR_KYRIE 4, PR_MAGNIFICAT 5, PR_GLORIA 3, PR_STRECOVERY 1, PR_KYRIE 10, PR_IMPOSITIO 3, PR_SANCTUARY 7, MG_SRECOVERY 5, ALL_RESURRECTION 4, PR_LEXDIVINA 5, PR_LEXAETERNA 1, HP_MEDITATIO 10, PR_ASPERSIO 4, MG_SAFETYWALL 8, PR_REDEMPTIO 1',
         skillsClasse3 => 'AB_CHEAL 3, AB_CANTO 3, AB_CLEMENTIA 3, AB_PRAEFATIO 10, AB_RENOVATIO 1, AB_HIGHNESSHEAL 5, AB_OFFERTORIUM 5',
         statsPadrao => '10 vit, 20 int, 15 dex, 20 vit, 25 dex, 30 int, 30 vit, 35 dex, 40 int, 45 vit, 50 int, 40 dex, 60 int, 75 vit, 95 int, 56 dex',
         statsPadraoTransclasse => '10 vit, 20 int, 15 dex, 20 vit, 25 dex, 30 int, 30 vit, 35 dex, 40 int, 45 vit, 50 int, 40 dex, 60 int, 75 vit, 95 int, 56 dex'
