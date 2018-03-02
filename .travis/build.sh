9c0bb106518ab14b4ec01189c0d3e34b8db50263
Vitor <vitor.silverio.rodrigues@gmail.com>
2018-03-02 10:02:14 -0300

    Inclusão dos plugins no pacote

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,12 @@ git fetch --unshallow
 
 jobs=("arcano" "arcebispo" "bioquimico" "cavaleiro-runico" "feiticeiro" "guardiao-real" "mecanico" "musa" "renegado" "sentinela" "sicario" "shura" "trovador")
 jobs_ok=(" arcebispo bioquimico guardiao-real renegado cavaleiro-runico ")
+plugins=("henrybk-plugins/BetterShopper" "andyfoss-plugins/by_me/runFromMonster")
 mkdir dist
+mkdir plugins
+for i in "${plugins[@]}"; do
+    cp -r submodule/$i plugins
+done
 for i in "${jobs[@]}"; do
     pwsh -File auxiliarGui.ps1 -job "$i"
     if [[ " {$jobs_ok[@]} " =~ " $i " ]]; then
@@ -12,9 +17,10 @@ for i in "${jobs[@]}"; do
     else 
         zip_file="$i.nao_testado.zip"
     fi    
-    zip $zip_file eventMacros.txt
+    zip -r $zip_file eventMacros.txt plugins
     mv $zip_file dist/ 
 done
+
 
 
 
