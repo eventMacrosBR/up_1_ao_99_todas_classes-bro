8daa01e71f337142540201ab3261211ddb3fffc2
Nipodemos <a.lanzi@hotmail.com>
2018-03-13 20:16:53 -0300

    mudança na quest luz divina

--- classes/arcebispo/quest-skill.pm
+++ classes/arcebispo/quest-skill.pm
@@ -5,11 +5,8 @@ automacro questLuzDivina_configurandoItens {
     exclusive 1
     call {
         do iconf 727 1 1 0 #Opala
-        do iconf Opala 1 1 0 #Opala
         do iconf 991 1 1 0 #Cristal Azul
-        do iconf Cristal Azul 1 1 0 #Cristal Azul
         do iconf 2608 1 1 0 #Rosário
-        do iconf Rosário 1 1 0 #Rosário
     }
 }
 
@@ -21,49 +18,25 @@ automacro questLuzDivina_inicioTenhoOsItens {
     ConfigKeyNot quest_eden terminando
     ConfigKeyNot In_saveMap_sequence true
     ConfigKeyNot virarClasse2 true
-    ConfigKeyNot quest_skill true
     InInventoryID 727 > 0
     InInventoryID 991 > 0
     InInventoryID 2608 > 0
     exclusive 1
-    call { 
-        [
-        log ===================================
-        log = Iniciando a quest da Luz Divina =
-        log = Tenho os itens.                 =
-        log ===================================
-        ]
-        call pararDeAtacar
-        do move prontera
-        do conf -f quest_skill true
-        do conf -f quest_luz_divina falar_com_npc
-    }
-}
-
-automacro questLuzDivina_inicioTenhoOsItens_Buged {
-    JobID $paramsClasses{idC1}, $paramsClasses{idC2}
-    SkillLevel AL_HOLYLIGHT != 1
-    JobLevel >= 30
-    ConfigKeyNot quest_eden em_curso
-    ConfigKeyNot quest_eden terminando
-    ConfigKeyNot In_saveMap_sequence true
-    ConfigKeyNot virarClasse2 true
-    ConfigKeyNot quest_skill true
-    InInventory "Opala" > 0
-    InInventory "Cristal Azul" > 0
-    InInventory "Rosário" > 0
-    exclusive 1
-    call { 
-        [
-        log ===================================
-        log = Iniciando a quest da Luz Divina =
-        log = Tenho os itens.                 =
-        log ===================================
-        ]
-        call pararDeAtacar
-        do move prontera
-        do conf -f quest_skill true
-        do conf -f quest_luz_divina falar_com_npc
+    call {
+        if (&invamount(Opala) > 0 && &invamount(Cristal Azul) > 0 && &invamount(Rosário) > 0) {
+            [
+            log ===================================
+            log = Iniciando a quest da Luz Divina
+            log = Tenho os itens.
+            log ===================================
+            ]
+            call pararDeAtacar
+            do move prontera
+            do conf -f quest_skill true
+            do conf -f quest_luz_divina falar_com_npc
+        } else {
+            log pequeno bug, ignorando
+        }
     }
 }
 
@@ -76,6 +49,7 @@ automacro questLuzDivina_inicioNaoTenhoOsItensMasTenhoZenys {
     ConfigKeyNot In_saveMap_sequence true
     ConfigKeyNot virarClasse2 true
     ConfigKeyNot quest_skill true
+    ConfigKeyNot quest_luz_divina falar_com_npc
     Zeny >= 36000
     exclusive 1
     call { 
@@ -95,69 +69,92 @@ automacro questLuzDivina_inicioNaoTenhoOsItensMasTenhoZenys {
 automacro questLuzDivina_comprarRosario {
     ConfigKey quest_skill true
     ConfigKey quest_luz_divina comprar_itens
+    ConfigKeyNot quest_luz_divina falar_com_npc
     InInventoryID 2608 < 1
     Zeny >= 16000
+    timeout 120
     exclusive 1
     priority -1
     call {
-        $qtRosario = &invamount (2608)
-        if ( $qtRosario < 1 ) {
+        call pararDeAtacar
+        $tenhoRosario = nao
+        $tenhoOpala = nao
+        $tenhoCristal = nao
+        
+        if (&invamount (2608)  < 1 ) { #Rosário
             do move prt_church 108 124
             do talk &npc(/Madre/)
             do store
             do buy &store(2608) 1
+        } else {
+            log Já tenho Rosário, indo comprar Opala
+            $tenhoRosario = sim
         }
-    }
-}
-
-#Caso não tenha conseguido um opala na caverna de payon
-automacro questLuzDivina_comprarOpala {
-    ConfigKey quest_skill true
-    ConfigKey quest_luz_divina comprar_itens
-    ConfigKeyNot BetterShopper_0 Opala
-    InInventoryID 727 < 1
-    Zeny >= 10000
-    exclusive 1
-    call {
-        call pararDeAtacar
-        do conf lockMap prontera
-        do conf route_randomWalk 1
-        do conf route_randomWalk_inTown 1
-        if (checarSeExisteNoConfig("BetterShopper_0") = nao) {
-            adicionaBetterShopper()
-            pause 1
-            do reload config
+        
+        if (&invamount(Opala) < 1) {
+            if (&config(BetterShopper_0) != Opala) {
+                [
+                log ===================================
+                log = Configurando pra comprar Opala
+                log ===================================
+                ]
+                do conf lockMap prontera
+                do conf route_randomWalk 1
+                do conf route_randomWalk_inTown 1
+                if (checarSeExisteNoConfig("BetterShopper_0") = nao) {
+                    adicionaBetterShopper()
+                    pause 1
+                    do reload config
+                }
+                do conf BetterShopper_0 Opala
+                do conf BetterShopper_0_maxPrice 10000
+                do conf BetterShopper_0_maxAmount 1
+                do conf BetterShopper_0_disabled 0
+                do conf -f BetterShopper_on 1
+            }
+        } else {
+            log Já tenho Opala
+            if (&config(BetterShopper_0_disabled) != 1) do conf BetterShopper_0_disabled 1
+            $tenhoOpala = sim
         }
-        do conf BetterShopper_0 Opala
-        do conf BetterShopper_0_maxPrice 10000
-        do conf BetterShopper_0_maxAmount 1
-        do conf BetterShopper_0_disabled 0
-        do conf -f BetterShopper_on 1
-    }
-}
-
-automacro questLuzDivina_comprarCristalAzul {
-    ConfigKey quest_skill true
-    ConfigKey quest_luz_divina comprar_itens
-    ConfigKeyNot BetterShopper_1 Cristal Azul
-    InInventoryID 991 < 1
-    Zeny >= 10000
-    exclusive 1
-    call {
-        call pararDeAtacar
-        do conf lockMap prontera
-        do conf route_randomWalk 1
-        do conf route_randomWalk_inTown 1
-        if (checarSeExisteNoConfig("BetterShopper_1") = nao) {
-            adicionaBetterShopper()
-            pause 1
-            do reload config
+        
+        if (&invamount(Cristal Azul) < 1) {
+            if (&config(BetterShopper_1) != Cristal Azul) {
+                do conf lockMap prontera
+                do conf route_randomWalk 1
+                do conf route_randomWalk_inTown 1
+                if (checarSeExisteNoConfig("BetterShopper_1") = nao) {
+                    adicionaBetterShopper()
+                    pause 1
+                    do reload config
+                }
+                do conf BetterShopper_1 Cristal Azul
+                do conf BetterShopper_1_maxPrice 10000
+                do conf BetterShopper_1_maxAmount 1
+                do conf BetterShopper_1_disabled 0
+                do conf -f BetterShopper_on 1
+            }
+        } else {
+            log Já tenho Cristal Azul
+            if (&config(BetterShopper_1_disabled) != 1) do conf BetterShopper_1_disabled 1
+            $tenhoCristal = sim
         }
-        do conf BetterShopper_1 Cristal Azul
-        do conf BetterShopper_1_maxPrice 10000
-        do conf BetterShopper_1_maxAmount 1
-        do conf BetterShopper_1_disabled 0
-        do conf -f BetterShopper_on 1
+        
+        if ( $tenhoRosario = sim && $tenhoOpala = sim && $tenhoCristal = sim ) {
+            [
+            log ===================================
+            log = TENHO TODOS OS ITENS, VOU FALAR COM NPC
+            log ===================================
+            ]
+            do conf -f quest_luz_divina falar_com_npc
+            do conf -f BetterShopper_on 0
+        } else {
+            [
+            log ===================================
+            log = ainda não tenho todos os itens para a quest
+            log ===================================
+            ]
+        }
     }
 }
 
