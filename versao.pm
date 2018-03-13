65921cdeb0a554add924dacad0e7da9821acc2fc
Nipodemos <a.lanzi@hotmail.com>
2018-03-13 20:18:43 -0300

    simplificado a macro versão

--- versao.pm
+++ versao.pm
@@ -5,13 +5,11 @@ automacro versao {
     call { 
         [ 
         log ================================================================= 
-        log   EventMacrosBR /               
-        log                  up_1_ao_99_todas_classes-bro  
+        log   EventMacrosBR / up_1_ao_99_todas_classes-bro  
         log   Versão: <versao>
         log   https://github.com/eventMacrosBR/up_1_ao_99_todas_classes-bro 
         log ================================================================= 
-        ] 
-        log Versão <versao> da macro
+        ]
         do conf -f versao_eventmacro_up_todas_as_classes_bro <versao>
     } 
 } 
