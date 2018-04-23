3872db930ce8976aacaddddab023593a691c0501
Vitor <vitor.silverio.rodrigues@gmail.com>
2018-04-22 23:17:11 -0300

    Ataque a distancia trovador

--- classes/trovador/config.pm
+++ classes/trovador/config.pm
@@ -47,3 +47,14 @@ sub extrairMapasDeUp {
     }
 }
 
+
+automacro configurarAtaqueADistancia {
+    ConfigKeyNot attackDistanceAuto 1
+    exclusive 1
+    JobIdNot 0
+    call {
+        do conf attackDistanceAuto 1
+    }
+}
+
+
