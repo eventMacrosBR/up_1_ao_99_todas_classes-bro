ed090d149a197d9ed5eda24e521f31ee8f359774
Vitor <vitor.silverio.rodrigues@gmail.com>
2018-04-26 10:37:03 -0300

    Movendo o uso da poção laranja apenas para as classes 2 que realemnte precisam

--- classes/cavaleiro-runico/skills-e-itens.pm
+++ classes/cavaleiro-runico/skills-e-itens.pm
@@ -64,4 +64,53 @@ automacro pegarPeco {
     }
 }
 
+automacro configurarPotLaranja {
+    InInventoryID 569 < 1 #Poção de Aprendiz (não pode ter essa poção)
+    InStorageID 569 < 1 #Poção de Aprendiz (não pode ter essa poção)
+    Zeny > 30000
+    BaseLevel != 99
+    JobID $paramsClasses{idC2}
+    ConfigKeyNot useSelf_item_0 Poção de Aprendiz #só se ativa quando nao ta usando mais pot aprendiz
+    ConfigKeyNot useSelf_item_0 Poção Laranja
+    exclusive 1
+    call {
+        [
+        log ===================================
+        log = configurando poção laranja
+        log ===================================
+        ]
+        do iconf 502 100 0 0 #pot laranja
+        if (&config(buyAuto_1) = Poção Laranja ) {
+            log pot Laranja já está configurada, essa mensagem não deveria aparecer
+            log caso apareça repetidamente, relate aos criadores da macro
+        } else {
+            adicionaBuyAuto() #preciso adicionar um bloco novo, porque o bloco
+            #de buyauto padrão não tem o "zeny" como chave, apesar que deveria
+            pause 1
+            do reload config 
+
+            do conf buyAuto_1 Poção Laranja
+            do conf buyAuto_1_maxAmount 100
+            do conf buyAuto_1_zeny > 30000
+            do conf buyAuto_1_npc payon_in01 5 49
+            do conf buyAuto_1_disabled 0
+        }
+        $blocoExiste = checarSeExisteNoConfig("useSelf_item_0")
+        if ( $blocoExiste = nao ) {
+            adicionaUseSelfItem()
+            pause 1
+            do reload config
+        }
+        do conf useSelf_item_0 Poção Laranja
+        do conf useSelf_item_0_hp < 50%
+        do conf useSelf_item_0_disabled 0
+
+        [
+        log ========================================
+        log Configação para usar e comprar Poção Laranja
+        log feita com sucesso
+        log ========================================
+        ]
+    }
+}
 
