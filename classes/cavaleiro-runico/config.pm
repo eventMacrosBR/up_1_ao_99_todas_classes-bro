13df2fa5d4bc26c35d3b2cb39b267af5e14b6c44
Vitor <vitor.silverio.rodrigues@gmail.com>
2018-05-09 18:22:17 -0300

    Build RK sugerida pelo @SarampoBolado

--- classes/cavaleiro-runico/config.pm
+++ classes/cavaleiro-runico/config.pm
@@ -6,7 +6,7 @@ sub configurarBuild {
         skillsClasse1 => 'Aumentar Recuperação de HP 10, Perícia com Espada 1, Perícia com Espada de Duas Mãos 10, Provocar 5, Vigor 8, Golpe Fulminante 10, Impacto Explosivo 5',
         skillsClasse2 => 'Rapidez com Duas Mãos 10, Montaria 1, Perícia em Montaria 5, Contra-Ataque 5, Impacto de Tyr 10, Perícia com Lança 10, Perfurar 8',
         skillsClasse1T => 'Aumentar Recuperação de HP 10, Perícia com Espada 1, Perícia com Espada de Duas Mãos 10, Provocar 5, Vigor 8, Golpe Fulminante 10, Impacto Explosivo 5',
-        skillsClasse2T => '',
+        skillsClasse2T => 'KN_RIDING 1, KN_CAVALIERMASTERY 5, KN_SPEARMASTERY 5, KN_PIERCE 5, KN_SPEARSTAB 5, LK_SPIRALPIERCE 5, KN_TWOHANDQUICKEN 10, LK_PARRYING 10, KN_AUTOCOUNTER 5, KN_BOWLINGBASH 4, LK_BERSERK 1, LK_TENSIONRELAX 1, LK_CONCENTRATION 5',
         skillsClasse3 => '',
         statsPadrao => '10 dex, 15 agi, 20 dex, 25 agi, 20 str, 30 dex, 60 agi, 30 str, 40 dex, 20 vit, 80 agi ,60 dex, 50 str, 90 agi, 70 str, 40 vit',
         statsPadraoTransclasse => '10 dex, 15 agi, 20 dex, 25 agi, 20 str, 30 dex, 60 agi, 30 str, 40 dex, 20 vit, 80 agi ,60 dex, 50 str, 90 agi, 70 str, 40 vit'
