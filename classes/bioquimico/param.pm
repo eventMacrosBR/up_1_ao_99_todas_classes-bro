dd21eec84e9628bb360a90c39b34ac2b889ab37e
deehtx <deehtx@live.com>
2018-05-09 22:52:35 -0300

    adicao de ids das classes babys no param.pm

--- classes/bioquimico/param.pm
+++ classes/bioquimico/param.pm
@@ -6,7 +6,10 @@ sub inicializarParametrosClasses {
         idC1T => 4006,
         idC2T => 4019,
         idC2TAlt => -2,
-        idC3 => 4071
+        idC3 => 4071,
+        idBC1 => 4031,
+        idBC2 => 4041,
+        idBC2Alt => -3
     );
     my $eventMacro = $eventMacro::Data::eventMacro;
     $eventMacro->set_full_hash('parametrosClasses', \%parametrosClasses);
--- classes/guardiao-real/param.pm
+++ classes/guardiao-real/param.pm
@@ -6,7 +6,10 @@ sub inicializarParametrosClasses {
         idC1T => 4002,
         idC2T => 4015,
         idC2TAlt => 4022,
-        idC3 => '4066,4073'
+        idC3 => '4066,4073',
+        idBC1 => 4024,
+        idBC2 => 4037,
+        idBC2Alt => 4044,
     );
     my $eventMacro = $eventMacro::Data::eventMacro;
     $eventMacro->set_full_hash('parametrosClasses', \%parametrosClasses);
