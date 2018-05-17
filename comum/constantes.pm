ffcb2ecb49f1ed530b8886b1394fbbafe14eb770
vitorsilverio <vitor.silverio.rodrigues@gmail.com>
2018-05-17 00:32:49 +0000

    Revert "exportando as constantes para macro toda"
    This reverts commit e43797fb74c1ed986dc1794184038c6bda47da82.

--- comum/constantes.pm
+++ comum/constantes.pm
@@ -1,17 +1,14 @@
 #Essa macro deve inicializar todas as constantes utilizadas pela eventMacro e acabar com boa parte dos MAGIC NUMBERS
-sub contantes_inicializar {
-    my $eventMacro = $eventMacro::Data::eventMacro;
-    
-
+macro contantes_inicializar {
     #Classes
-    $eventMacro->set_scalar_var("APRENDIZ", 0 , 0);
-    $eventMacro->set_scalar_var("APRENDIZ_T", 4001, 0);
-    $eventMacro->set_scalar_var("APRENDIZ_BABY", 4023, 0);
+    $APRENDIZ = 0
+    $APRENDIZ_T = 4001
+    $APRENDIZ_BABY = 4023
 
     #Valores
-    $eventMacro->set_scalar_var("INDEFINIDO", -1, 0);
-    $eventMacro->set_scalar_var("TRUE", 1, 0);
-    $eventMacro->set_scalar_var("FALSE", 0, 0);
+    $INDEFINIDO = -1
+    $TRUE = 1
+    $FALSE = 0
 
     #Equipes
     #Aprendiz
