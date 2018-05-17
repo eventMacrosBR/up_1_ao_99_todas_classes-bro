e43797fb74c1ed986dc1794184038c6bda47da82
Vitor <vitor.silverio.rodrigues@gmail.com>
2018-05-16 21:29:13 -0300

    exportando as constantes para macro toda

--- comum/constantes.pm
+++ comum/constantes.pm
@@ -1,14 +1,17 @@
 #Essa macro deve inicializar todas as constantes utilizadas pela eventMacro e acabar com boa parte dos MAGIC NUMBERS
-macro contantes_inicializar {
+sub contantes_inicializar {
+    my $eventMacro = $eventMacro::Data::eventMacro;
+    
+
     #Classes
-    $APRENDIZ = 0
-    $APRENDIZ_T = 4001
-    $APRENDIZ_BABY = 4023
+    $eventMacro->set_scalar_var("APRENDIZ", 0 , 0);
+    $eventMacro->set_scalar_var("APRENDIZ_T", 4001, 0);
+    $eventMacro->set_scalar_var("APRENDIZ_BABY", 4023, 0);
 
     #Valores
-    $INDEFINIDO = -1
-    $TRUE = 1
-    $FALSE = 0
+    $eventMacro->set_scalar_var("INDEFINIDO", -1, 0);
+    $eventMacro->set_scalar_var("TRUE", 1, 0);
+    $eventMacro->set_scalar_var("FALSE", 0, 0);
 
     #Equipes
     #Aprendiz
