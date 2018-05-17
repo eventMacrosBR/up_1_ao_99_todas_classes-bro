1f994f4d917b7ad0083da8b7a725fc30cc882b02
Alan William <nipodemos13@gmail.com>
2018-05-17 01:45:54 -0300

    alterado nome da kafra que esqueci

--- comum/salvar-na-cidade.pm
+++ comum/salvar-na-cidade.pm
@@ -200,7 +200,7 @@ automacro SalvoNaKafra {
     exclusive 1
     CheckOnAI manual
     priority -5
-    NpcMsgName /O seu Ponto (de Retorno )?foi salvo|saved here|foi salvo aqui na cidade|foi salvo em|foi salvo aqui/ /Kafra Employee|Funcionária Kafra|Corp/
+    NpcMsgName /O seu Ponto (de Retorno )?foi salvo|saved here|foi salvo aqui na cidade|foi salvo em|foi salvo aqui/ /Kafra Employee|Funcionária Kafra|Corp|Kafra Recepcionista/
     ConfigKeyNot saveMap $saveMap
     ConfigKey naSequenciaDeSalvamento true
     InMap $saveMap
