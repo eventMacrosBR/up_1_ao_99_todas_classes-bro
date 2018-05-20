f13585e9510e0c1ae3b850e2550e6814187caf48
Alan William <nipodemos13@gmail.com>
2018-05-20 12:53:38 -0300

    adicionado comentários para facilitar entendimento do plugins.pm

--- comum/plugins.pm
+++ comum/plugins.pm
@@ -1,6 +1,6 @@
 sub verificarEInstalarPlugin {
     my ($plugin) = @_;
-    if (-e "plugins/$plugin.pl") {
+    if (-e "plugins/$plugin.pl") { #primeiro checa se o plugin já está na pasta plugins do opk
 
         $estaNoSys = 0;
         
@@ -9,12 +9,14 @@ sub verificarEInstalarPlugin {
         my @lines = <FILE>;
         close(FILE);
         chomp @lines;
+        #como ele está na pasta, agora realiza a verificação se ele está ativado no sys.txt
         foreach my $line (@lines) {
             next if $line =~ /^$/ || $line =~ /^#/;
             if ($line =~ /$plugin/i ) {
                 $estaNoSys = 1;
             }
         }
+        #se não estiver ativado, esse código ativa o plugin no sys.txt automaticamente
         if ( !$estaNoSys ) {
             foreach my $line (@lines) {
                 next if $line =~ /^$/ || $line =~ /^#/;
@@ -28,12 +30,14 @@ sub verificarEInstalarPlugin {
             message "Ativando $plugin\n";
             Commands::run("plugin load plugins/$plugin.pl");
         } else {
+            #essa mensagem aparece quando o plugin está na pasta correta, e está ativado no sys.txt
             message "Checando plugin $plugin...\nTudo certo.\n";
         }
     } else {
+        #erro caso a pessoa não tiver instalado o plugin necessário para o funcionamento
         error "Plugin $plugin não está na pasta plugins\n";
         error "Por favor coloque o $plugin.pl na pasta plugins\n";
-        Misc::offlineMode();
+        Misc::offlineMode(); #interrompe o openkore de continuar, para que a pessoa possa ver o erro
     }
 }
 
