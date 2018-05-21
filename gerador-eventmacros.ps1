37ee0453c910e9b7b04b8fec477523615d86efc9
Vitor <vitor.silverio.rodrigues@gmail.com>
2018-05-21 01:32:58 -0300

    Novo gerador com Personalizador de build

--- gerador-eventmacros.ps1
+++ gerador-eventmacros.ps1
@@ -1,4 +1,4 @@
-﻿param ( [string]$job )
+param ( [string]$job )
 
 if (! $job) {
     Add-Type -AssemblyName System.Windows.Forms
@@ -8,6 +8,23 @@ if (! $job) {
     $listJobs = New-Object system.windows.Forms.ListView
     $btn = New-Object system.windows.Forms.Button
     $imageListIcons = New-Object System.Windows.Forms.ImageList
+    $labelConfigsPersonalizadas = New-Object System.Windows.Forms.Label
+    $configsPersonalizadas = New-Object System.Windows.Forms.PropertyGrid
+    $labelClasseSelecionada = New-Object System.Windows.Forms.Label
+
+    Class Configuracoes {
+        [string]$skills_classe_1
+        [string]$skills_classe_2
+        [string]$skills_classe_1_transclasse
+        [string]$skills_classe_2_transclasse
+        [string]$skills_classe_3
+        [string]$stats_nao_transclasse
+        [string]$stats_transclasse
+        [string]$stats_classe_3
+
+    }
+    
+
 }
 
 function getVersao {
@@ -46,35 +63,156 @@ function gerarMacro {
     Get-Content -Encoding UTF8 comum\*.pm | Out-File $eventMacros -Encoding UTF8 -append
 }
 
+function salvarBuild {
+    param ($classe)
+    $arquivo = "classes/$classe/config.pm"
+    $config = $configsPersonalizadas.SelectedObject
+    $tempFile = "classes/$classe/config.pm.tmp"
+    foreach($line in Get-Content -Encoding UTF8 $arquivo) {
+        if($line -match "^\s+\bskillsClasse1\b.*"){
+            $c = $config.skills_classe_1
+            "        skillsClasse1 => '$c'," | Out-File $tempFile -Encoding UTF8 -append
+        } else {
+            if($line -match "^\s+\bskillsClasse2\b.*"){
+                $c = $config.skills_classe_2
+                "        skillsClasse2 => '$c'," | Out-File $tempFile -Encoding UTF8 -append
+            } else {
+                if($line -match "^\s+\bskillsClasse1T\b.*"){
+                    $c = $config.skills_classe_1_transclasse
+                    "        skillsClasse1T => '$c'," | Out-File $tempFile -Encoding UTF8 -append
+                } else {
+                    if($line -match "^\s+\bskillsClasse2T\b.*"){
+                        $c = $config.skills_classe_2_transclasse
+                        "        skillsClasse2T => '$c'," | Out-File $tempFile -Encoding UTF8 -append
+                    } else { 
+                        if($line -match "^\s+\bskillsClasse3\b.*"){
+                            $c = $config.skills_classe_3
+                            "        skillsClasse3 => '$c'," | Out-File $tempFile -Encoding UTF8 -append
+                        } else {
+                            if($line -match "^\s+\bstatsPadrao\b.*"){
+                                $c = $config.stats_nao_transclasse 
+                                "        statsPadrao => '$c'," | Out-File $tempFile -Encoding UTF8 -append
+                            } else {
+                                if($line -match "^\s+\bstatsPadraoTransclasse\b.*"){
+                                    $c = $config.stats_transclasse
+                                    "        statsPadraoTransclasse => '$c'," | Out-File $tempFile -Encoding UTF8 -append
+                                } else {
+                                    if($line -match "^\s+\bstatsPadraoClasse3\b.*"){
+                                        $c = $config.stats_classe_3
+                                        "        statsPadraoClasse3 => '$c'" | Out-File $tempFile -Encoding UTF8 -append
+                                    } else{
+                                        $line | Out-File $tempFile -Encoding UTF8 -append
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    Remove-Item $arquivo
+    Rename-Item -Path $tempFile -NewName "config.pm"
+
+}
+
 function acaoBotaoGerar {
     $classe = $listJobs.SelectedItems
     if ($classe.Count -eq 1) {
-        gerarMacro($classe[0].Text)
-        [System.Windows.Forms.MessageBox]::Show("eventMacros.txt para "+$classe[0].Text+" gerado com sucesso!" , "Ok")
+        $classeSelecionada = $classe[0].Text
+        salvarBuild($classeSelecionada)
+        gerarMacro($classeSelecionada)
+        [System.Windows.Forms.MessageBox]::Show("eventMacros.txt para $classeSelecionada gerado com sucesso!" , "Ok")
         $Form.Dispose()
     } else{
         [System.Windows.Forms.MessageBox]::Show("Erro, nenhum item selecionado", "Selecione uma classe")
     }
 }
 
+function acaoCarregarConfiguracoes {
+    $classe = $listJobs.SelectedItems
+    if ($classe.Count -eq 1) {
+        $classeSelecionada = $classe[0].Text
+        Write-Host "Classe selecionada: $classeSelecionada"
+
+        $labelClasseSelecionada.Text = "Classe selecionada: $classeSelecionada"
+        $c = limparNomeDaClasse($classe[0].Text)
+        $config = [Configuracoes]::new()
+        $arquivo = "classes/$c/config.pm"
+        Write-Host "Abrindo arquivo: $arquivo"
+        foreach($line in Get-Content -Encoding UTF8 $arquivo) {
+
+            if($line -match "^\s+\bskillsClasse1\b.*"){
+                $config.skills_classe_1 = $line -replace ".*'(.*)'.*",'$1'
+            }
+            if($line -match "^\s+\bskillsClasse2\b.*"){
+                $config.skills_classe_2 = $line -replace ".*'(.*)'.*",'$1'
+            }
+            if($line -match "^\s+\bskillsClasse1T\b.*"){
+                $config.skills_classe_1_transclasse = $line -replace ".*'(.*)'.*",'$1'
+            }
+            if($line -match "^\s+\bskillsClasse2T\b.*"){
+                $config.skills_classe_2_transclasse = $line -replace ".*'(.*)'.*",'$1'
+            }
+            if($line -match "^\s+\bskillsClasse3\b.*"){
+                $config.skills_classe_3 = $line -replace ".*'(.*)'.*",'$1'
+            }
+            if($line -match "^\s+\bstatsPadrao\b.*"){
+                $config.stats_nao_transclasse = $line -replace ".*'(.*)'.*",'$1'
+            }
+            if($line -match "^\s+\bstatsPadraoTransclasse\b.*"){
+                $config.stats_transclasse = $line -replace ".*'(.*)'.*",'$1'
+            }
+            if($line -match "^\s+\bstatsPadraoClasse3\b.*"){
+                $config.stats_classe_3 = $line -replace ".*'(.*)'.*",'$1'
+            }
+        }
+        $configsPersonalizadas.SelectedObject = $config
+
+    } else {
+        $labelClasseSelecionada.Text = "Classe selecionada: "
+        $configsPersonalizadas.SelectedObject = [Configuracoes]::new()
+    }
+}
+
+
 function desenharJanela {
     $versao = getVersao
     $Form.Text = "Gerador eventMacros.txt versão: " + $versao
     $Form.TopMost = $true
-    $Form.Width = 450
-    $Form.Height = 400
-    
-    $listJobs.Width = 300
-    $listJobs.Height = 300
-    $listJobs.location = new-object system.drawing.point(10,20)
+    $Form.Width = 800
+    $Form.Height = 600
+
+    $listJobs.Width = 760
+    $listJobs.Height = 130
+    $listJobs.location = New-Object system.drawing.point(10,20)
     $Form.controls.Add($listJobs)
     $listJobs.View = "LargeIcon"
     $listJobs.LargeImageList = $imageListIcons
     $listJobs.MultiSelect = 0
+    $listJobs.Add_click({ acaoCarregarConfiguracoes })
+
+    $labelClasseSelecionada.Width = 400
+    $labelClasseSelecionada.Text = "Classe selecionada: "
+    $labelClasseSelecionada.Location = New-Object system.drawing.point(10,160)
+    $Form.controls.Add($labelClasseSelecionada)
+
+    $labelConfigsPersonalizadas.Text = "Configurações Personalizadas"
+    $labelConfigsPersonalizadas.Width = 400
+    $labelConfigsPersonalizadas.location = New-Object system.drawing.point(10,195)
+    $Form.controls.Add($labelConfigsPersonalizadas)
+ 
+    $configsPersonalizadas.Width = 760
+    $configsPersonalizadas.Height = 300
+    $configsPersonalizadas.location = New-Object system.drawing.point(10,220)
+    $configsPersonalizadas.SelectedObject = [Configuracoes]::new() 
+    $configsPersonalizadas.AutoSize = true
+    $Form.controls.Add($configsPersonalizadas)
+
     
     $btn.Text = "Gerar"
     $btn.Width = 60
-    $btn.location = new-object system.drawing.point(310,20)
+    $btn.location = New-Object system.drawing.point(710,520)
     $Form.controls.Add($btn)
     $Form.AcceptButton = $btn
 
@@ -91,6 +229,7 @@ function carregarValores {
         $imageListIcons.Images.Add([System.Drawing.Image]::FromFile("gerador-images/$classe.png"))
         $listItemClasse.ImageIndex = $i
         $listItemClasse.Text = $classes[$i]
+        
         $listJobs.Items.Add($listItemClasse)
     } 
 }
