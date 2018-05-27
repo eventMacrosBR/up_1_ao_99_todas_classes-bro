53030d3856f07bf624c58a2661f2a1a0476413a7
lksoliver <lksoliver13@gmail.com>
2018-05-27 03:30:48 -0300

    Detalhes que não encherguei

--- classes/arcano/config.pm
+++ classes/arcano/config.pm
@@ -5,10 +5,10 @@ sub configurarBuild {
         skillsClasse1 => 'MG_NAPALMBEAT 5, MG_SOULSTRIKE 5, MG_LIGHTNINGBOLT 5, MG_FIREBOLT 5, MG_COLDBOLT 5, MG_SRECOVERY 5, MG_FIREBOLT 7, MG_COLDBOLT 7, MG_FIREBOLT 10, MG_COLDBOLT 10, MG_SOULSTRIKE 10, MG_STONECURSE 1, MG_SIGHT 1, MG_FROSTDIVER 1, MG_THUNDERSTORM 1',
         skillsClasse2 => 'WZ_ICEWALL 5, WZ_FROSTNOVA 10, WZ_WATERBALL 5, WZ_STORMGUST 1, WZ_JUPITEL 1, WZ_VERMILION 1, WZ_EARTHSPIKE 5, WZ_EARTHSPIKE 1, WZ_QUAGMIRE 5, WZ_SIGHTRASHER 2, WZ_METEOR 9',
         skillsClasse1T => 'MG_LIGHTNINGBOLT 4, MG_COLDBOLT 10, MG_SRECOVERY 7, MG_NAPALMBEAT 7, MG_SOULSTRIKE 7, MG_SAFETYWALL 10, MG_STONECURSE 1, MG_SIGHT 1, MG_FROSTDIVER 1, MG_THUNDERSTORM 1',
-        skillsClasse2T => 'HW_MAGICPOWER 10, HW_SOULDRAIN 10, WZ_ESTIMATION 1, WZ_ICEWALL 5, WZ_FROSTNOVA 10, WZ_WATERBALL 5, WZ_STORMGUST 10, WZ_JUPITEL 5, WZ_VERMILION 1, WZ_EARTHSPIKE 3, WZ_HEAVENDRIVE 1, WZ_QUAGMIRE 5, WZ_SIGHTRASHER 2, WZ_METEOR 1'
+        skillsClasse2T => 'HW_MAGICPOWER 10, HW_SOULDRAIN 10, WZ_ESTIMATION 1, WZ_ICEWALL 5, WZ_FROSTNOVA 10, WZ_WATERBALL 5, WZ_STORMGUST 10, WZ_JUPITEL 5, WZ_VERMILION 1, WZ_EARTHSPIKE 3, WZ_HEAVENDRIVE 1, WZ_QUAGMIRE 5, WZ_SIGHTRASHER 2, WZ_METEOR 1',
         skillsClasse3 => '',
-        statsPadrao => '20 dex, 40 int, 10 vit, 20 luk, 40 dex, 70 int, 40 luk, 70 dex, 60 luk, 99 int',
-        statsPadraoTransclasse => '15 dex, 15 int, 15 vit, 30 dex, 30 int, 25 vit, 60 int, 60 dex, 90 int, 90 dex, 99 int, 99 dex',
+        statsPadrao => '10 dex, 15 agi, 20 dex, 25 agi, 20 str, 30 dex, 60 agi, 30 str, 40 dex, 20 vit, 80 agi ,60 dex, 50 str, 90 agi, 70 str, 40 vit',
+        statsPadraoTransclasse => '10 dex, 15 agi, 20 dex, 25 agi, 20 str, 30 dex, 60 agi, 30 str, 40 dex, 20 vit, 80 agi ,60 dex, 50 str, 90 agi, 70 str, 40 vit',
         statsPadraoClasse3 => ''
     );
     my $eventMacro = $eventMacro::Data::eventMacro;
