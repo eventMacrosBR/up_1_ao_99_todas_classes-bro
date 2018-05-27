188e271ed6165e1f57a3f4963b5c7aa5a2785d1b
Vitor <vitor.silverio.rodrigues@gmail.com>
2018-05-26 21:37:37 -0300

    Travis corrigindo plugins e adicionando novas classes

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,15 +4,10 @@ git fetch --unshallow
 git checkout -b master
 git submodule update --init --recursive
 
-jobs=("arcano" "arcebispo" "bioquimico" "cavaleiro-runico" "feiticeiro" "guardiao-real" "mecanico" "musa" "renegado" "sentinela" "sicario" "shura" "trovador")
+jobs=("arcano" "arcebispo" "bioquimico" "cavaleiro-runico" "feiticeiro" "guardiao-real" "mecanico" "musa" "renegado" "sentinela" "sicario" "shura" "trovador" "mestre-taekwon" "espiritualista" "kagerou" "oboro" "justiceiro" "superaprendiz")
 jobs_ok=(" arcebispo bioquimico guardiao-real renegado cavaleiro-runico sentinela ")
-plugins=("henrybk-plugins/BetterShopper" "andyfoss-plugins/by_me/runFromMonster")
 mkdir dist
-mkdir plugins
-for i in "${plugins[@]}"; do
-    cp -r submodules/$i plugins/
-    for i in plugins; do zip -r dist/$i.zip $i; done  
-done
+zip -r dist/plugins.zip PluginsParaUpTodasAsClasses/
 for i in "${jobs[@]}"; do
     pwsh -File gerador-eventmacros.ps1 -job "$i"
     if [[ " {$jobs_ok[@]} " =~ " $i " ]]; then
