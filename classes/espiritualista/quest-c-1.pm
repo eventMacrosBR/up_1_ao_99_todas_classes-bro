370d0981e648793db8b631c891da714a54f0f59e
Nipodemos <nipodemos13@gmail.com>
2018-05-28 03:18:27 -0300

    começo da quest de taekwon (ainda nao funcional)

--- classes/espiritualista/quest-c-1.pm
+++ classes/espiritualista/quest-c-1.pm
@@ -1,9 +1,36 @@
 sub inicializarParametrosQuestClasse1 {
-     Commands::run("eventMacro disable questClasse1_virandoClasse1");
-     my %parametrosQuestClasse1 = (
-        equipeIniciante => 0
+    Commands::run("eventMacro disable questClasse1_virandoClasse1");
+    my %parametrosQuestClasse1 = (
+        nomeClasse => "arqueiro", #isso é gambiarra pra ele sair do campo de aprendiz e ir pra payon
+        equipeIniciante => -1,
+        mapa => 'payon_in02',
     );
     my $eventMacro = $eventMacro::Data::eventMacro;
     $eventMacro->set_full_hash('parametrosQuestClasse1', \%parametrosQuestClasse1);
 }
 
+
+#6000#Mudança de Classe: Taekwon#SG_FEEL#QUE_NOIMAGE#
+#Você escolheu o caminho do Taekwon. Você deve aumentar seu nível de base e falar com Fênix. #
+#
+
+#6001#Mudança de Classe: Taekwon#SG_FEEL#QUE_NOIMAGE#
+#Você escolheu o caminho do Taekwon. Você deve passar pelo exame de aptidão mental dado por Fênix. #
+#
+
+#6002#Mudança de Classe: Taekwon#SG_FEEL#QUE_NOIMAGE#
+#Você finalmente pode se tornar Taekwon, falando mais uma vez com Fênix. #
+#
+
+#automacro questTaekwon_começo_npcLonge {
+#    exclusive 1
+#    JobID 0, 4023
+#    InMap payon_in02,payon,pay_arche
+#    QuestInactive 6000
+#    QuestInactive 6001
+#    QuestInactive 6002
+#    call {
+#        do move payon 157 141 &rand(2,7)
+#        do talknpc 157 141 
+#    }
+#}
