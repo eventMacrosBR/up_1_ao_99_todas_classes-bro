2776515a3942e770fc92384818870aeb2ed99959
Nipodemos <nipodemos13@gmail.com>
2018-06-12 12:52:22 -0300

    alteração para classes comprar pot nos lvl certos
    Obs: classes que precisam juntar dinehiro para fazer a quest de classe 2
    não vão comprar pot laranja enquanto forem
    de classe 1. Os que não precisam economizar compram
    desde que não sejam aprendizes (que bugaria campo de aprendiz)
    
    classes que não tem muita força, foram programados
    para comprar apenas 50 pots em vez de 100

--- classes/cavaleiro-runico/skills-e-itens.pm
+++ classes/cavaleiro-runico/skills-e-itens.pm
@@ -71,7 +71,10 @@ automacro configurarPotLaranja {
     InStorageID 569 < 1 #Poção de Aprendiz (não pode ter essa poção)
     Zeny > 30000
     BaseLevel != 99
-    JobID $parametrosClasses{idC2}, $parametrosClasses{idBC2}, $parametrosClasses{idC2Alt}, $parametrosClasses{idBC2Alt}
+    JobIDNot 0 #aprendiz
+    JobIDNot 4023 # baby aprendiz
+    ConfigKey quest_skill none
+    ConfigKey questRenascer_estagio none
     ConfigKeyNot useSelf_item_0 Poção de Aprendiz #só se ativa quando nao ta usando mais pot aprendiz
     ConfigKeyNot useSelf_item_0 Poção Laranja
     exclusive 1
@@ -81,6 +84,27 @@ automacro configurarPotLaranja {
         log = configurando poção laranja
         log ===================================
         ]
+        if (&invamount(569) != 0 || &storamount(569) != 0) {
+            [
+            log ===================================
+            log = um bug ocorreu
+            log = a eventMacro está tentando desativar as pot aprendiz
+            log = mas ainda tenho um pouco delas
+            log = tenho &invamount(569) no inventário
+            log = e tenho &storamount(569) no storage
+            log = então vou continuar usando
+            log = reativando useSelf_item Poção de Aprendiz
+            log ===================================
+            ]
+            do conf useSelf_item_0 Poção de Aprendiz
+            do conf useSelf_item_0_disabled 0
+            do conf useSelf_item_0_hp < 60%
+            do conf getAuto_0 Poção de Aprendiz
+            do conf getAuto_0_minAmount 20
+            do conf getAuto_0_maxAmount 200
+            do conf getAuto_0_passive 0
+            stop
+        }
         do iconf 502 100 0 0 #pot laranja
         if (&config(buyAuto_1) = Poção Laranja ) {
             log pot Laranja já está configurada, essa mensagem não deveria aparecer
@@ -92,6 +116,7 @@ automacro configurarPotLaranja {
             do reload config 
 
             do conf buyAuto_1 Poção Laranja
+            do conf buyAuto_1_minAmount 10
             do conf buyAuto_1_maxAmount 100
             do conf buyAuto_1_zeny > 30000
             do conf buyAuto_1_npc payon_in01 5 49
@@ -113,6 +138,30 @@ automacro configurarPotLaranja {
         log feita com sucesso
         log ========================================
         ]
+    }
+}
+
+#só vai reabilitar o buyAuto se não tiver fazendo nada!!!
+#upando ou coisa assim
+automacro reabilitarBuyAuto {
+    BaseLevel != 99
+    ConfigKeyNot quest_eden em_curso
+    ConfigKeyNot quest_eden terminando
+    ConfigKeyNot naSequenciaDeSalvamento true
+    ConfigKeyNot virarClasse2 true
+    ConfigKeyNot virarClasse2T true
+    ConfigKeyNot quest_skill true
+    ConfigKeyNot esperarFazerQuest true
+    ConfigKey aeroplano1 none
+    ConfigKey aeroplano2 none
+    ConfigKey questRenascer_estagio none
+    exclusive 1
+    JobIDNot 0 #Ou o campo de treinamento fica louco
+    JobIDNot 4023 #Baby Aprendiz
+    ConfigKey buyAuto_1 Poção Laranja
+    ConfigKeyNot buyAuto_1_disabled 0
+    call {
+        do conf buyAuto_1_disabled 0
     }
 }
 
