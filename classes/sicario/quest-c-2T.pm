4a1d699fe4f5015f5357483dcf5b9a13a9a0296d
Nipodemos <a.lanzi@hotmail.com>
2018-06-28 01:03:03 -0300

    corrigindo erro de sicario
    fix #138
    fix 138

---
+++ classes/sicario/quest-c-2T.pm
@@ -0,0 +1,8 @@
+sub inicializarParametrosQuestClasse2T {
+    my %parametrosQuestClasse2T = (
+        Npc => '44 58'
+    );
+    my $eventMacro = $eventMacro::Data::eventMacro;
+    $eventMacro->set_full_hash('parametrosQuestClasse2T', \%parametrosQuestClasse2T);
+}
+
